93f47285b8416bdb3a7326db9344027e1e43f234
Grigory Entin <grigory.entin@gmail.com>
2015-01-04 22:15:30 +0300

    Added importItemsFromJson, importItemsFromJsonData; made imported objects returned along the error.

--- RSSReader/GenericCoreDataExtensions.swift
+++ RSSReader/GenericCoreDataExtensions.swift
@@ -8,17 +8,21 @@
 
 import CoreData
 
+let GenericCoreDataExtensionsErrorDomain = "com.grigoryentin.GenericCoreDataExtensions"
+enum GenericCoreDataExtensionsError: Int {
+	case JsonElementNotFoundOrInvalid
+}
+
 protocol ManagedIdentifiable {
 	var id: String { get }
 	class func entityName() -> String
 	func importFromJson(jsonObject: AnyObject)
 }
 
-func importJson<T: ManagedIdentifiable>(cls: T.Type, json: NSDictionary, #managedObjectContext: NSManagedObjectContext) -> NSError? {
+func importJson<T: ManagedIdentifiable>(cls: T.Type, json: [String: AnyObject], #managedObjectContext: NSManagedObjectContext, error: NSErrorPointer) -> T? {
 	let id = json["id"] as NSString
-	var existingObject: T?
 	let entityName = cls.entityName()
-	let errorForExistingObject: NSError? = {
+	let (existingObject: T?, errorForExistingObject: NSError?) = {
 		let request: NSFetchRequest = {
 			let $ = NSFetchRequest(entityName: entityName)
 			$.predicate = NSPredicate(format: "id == %@", argumentArray: [id])
@@ -26,26 +30,65 @@ func importJson<T: ManagedIdentifiable>(cls: T.Type, json: NSDictionary, #manage
 			return $
 		}()
 		var fetchError: NSError?
-		if let objects = managedObjectContext.executeFetchRequest(request, error: &fetchError) {
-			if objects.count > 0 {
-				existingObject = (objects[0] as T)
-			}
-			else {
-				existingObject = nil
-			}
-			return nil
+		let objects = managedObjectContext.executeFetchRequest(request, error: &fetchError)
+		if nil == objects {
+			return (nil, trace("fetchError", fetchError))
 		}
-		else {
-			return fetchError
-		}
+		let existingObject = objects?.last as T?
+		return (existingObject, nil)
 	}()
 	if let errorForExistingObject = errorForExistingObject {
-		return errorForExistingObject
+		error.memory = errorForExistingObject
+		return nil
 	}
 	let object: T = existingObject ?? {
 		let newObject = NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: managedObjectContext) as T
 		return newObject
 	}()
 	object.importFromJson(json)
-	return nil
+	return object
+}
+func importItemsFromJson<T: ManagedIdentifiable>(json: [String : AnyObject], #type: T.Type, #elementName: NSString, #managedObjectContext: NSManagedObjectContext, #error: NSErrorPointer) -> [T]? {
+	var items = [T]()
+	let completionError: NSError? = {
+		let itemJsons = json[elementName] as? [[String : AnyObject]]
+		if nil == itemJsons {
+			let jsonElementNotFoundOrInvalidError = NSError(domain: GenericCoreDataExtensionsErrorDomain, code: GenericCoreDataExtensionsError.JsonElementNotFoundOrInvalid.rawValue, userInfo: nil)
+			return trace("jsonElementNotFoundOrInvalidError", jsonElementNotFoundOrInvalidError)
+		}
+		for itemJson in itemJsons! {
+			var importItemError: NSError?
+			let item = importJson(type, itemJson, managedObjectContext: managedObjectContext, &importItemError)
+			if nil == item {
+				return trace("importItemError", importItemError)
+			}
+			items += [item!]
+		}
+		return nil
+	}()
+	if let completionError = completionError {
+		error.memory = trace("completionError", completionError)
+		return nil
+	}
+	return items
+}
+func importItemsFromJsonData<T: ManagedIdentifiable>(data: NSData, #type: T.Type, #elementName: NSString, #managedObjectContext: NSManagedObjectContext, #error: NSErrorPointer) -> [T]? {
+	let (json: [String : AnyObject]?, jsonError: NSError?) = {
+		var jsonParseError: NSError?
+		let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(), error: &jsonParseError)
+		if nil == jsonObject {
+			return (nil, trace("jsonParseError", jsonParseError))
+		}
+		let json = jsonObject as? [String : AnyObject]
+		if nil == json {
+			let jsonIsNotDictionaryError = NSError()
+			return (json, trace("jsonIsNotDictionaryError", jsonIsNotDictionaryError))
+		}
+		return (json, nil)
+	}()
+	if let jsonError = jsonError {
+		error.memory = trace("jsonError", jsonError)
+		return nil
+	}
+	return importItemsFromJson(json!, type: type, elementName: elementName, managedObjectContext: managedObjectContext, error: error)
 }
