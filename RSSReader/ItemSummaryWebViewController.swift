7fa8600a2291f9a193c44a9639cebf8004a70d9a
Grigory Entin <grigory.entin@gmail.com>
2015-01-05 03:13:52 +0300

    Switched to ItemSummary.css/Template.html.

--- RSSReader/ItemSummaryWebViewController.swift
+++ RSSReader/ItemSummaryWebViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ItemSummaryWebViewController: UIViewController {
-	@IBOutlet var textView: UITextView!
 	@IBOutlet var webView: UIWebView!
 	var item: Item!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		self.webView.loadHTMLString(item.summary, baseURL: NSURL(string:"http://localhost"))
-		self.title = item.title
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let bundle = NSBundle.mainBundle()
+		let htmlTemplateURL = bundle.URLForResource("ItemSummaryTemplate", withExtension: "html")!
+		var htmlTemplateLoadError: NSError?
+		let htmlTemplate = NSString(contentsOfURL: htmlTemplateURL, encoding: NSUTF8StringEncoding, error: &htmlTemplateLoadError)!
+		let htmlString =
+			htmlTemplate
+				.stringByReplacingOccurrencesOfString("$$Summary$$", withString: item.summary!)
+				.stringByReplacingOccurrencesOfString("$$Title$$", withString: item.title!)
+		self.webView.loadHTMLString(htmlString, baseURL: bundle.resourceURL)
+	}
 }
