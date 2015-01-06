69a13ae9110a657167aea7a914eb02cb68e86c82
Grigory Entin <grigory.entin@gmail.com>
2015-01-06 15:39:07 +0300

    Added .category; fixed table view cell expected to exist on fetched results .Update; disabled fetched results cache.

--- RSSReader/SubscriptionsListViewController.swift
+++ RSSReader/SubscriptionsListViewController.swift
@@ -10,42 +10,48 @@ import UIKit
 import CoreData
 
 class SubscriptionsListViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-
+	var category: Folder?
 	lazy var fetchedResultsController: NSFetchedResultsController = {
 		let fetchRequest: NSFetchRequest = {
 			let $ = NSFetchRequest(entityName: Subscription.entityName())
-			$.sortDescriptors = [NSSortDescriptor(key: "sortID", ascending: true)]
+			$.sortDescriptors = Subscription.sortDescriptorsVariants()[0]
+			if let category = self.category {
+				$.predicate = NSPredicate(format: "categories CONTAINS %@", argumentArray: [category])
+			}
 			return $
 		}()
-		let cacheName = "Cache"
-		let $ = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.mainQueueManagedObjectContext, sectionNameKeyPath: nil, cacheName: cacheName)
+		let $ = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.mainQueueManagedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
 		$.delegate = self
 		return $
 	}()
+	// MARK: -
 	@IBAction func refresh(sender: AnyObject!) {
 		rssSession.updateSubscriptions { (error: NSError?) -> Void in
 			let refreshControl = sender as UIRefreshControl
 			refreshControl.endRefreshing()
 		}
 	}
+	// MARK: -
 	func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
 		let subscription = fetchedResultsController.fetchedObjects![indexPath.row] as Subscription
 		cell.textLabel?.text = subscription.title ?? subscription.url?.lastPathComponent
 	}
+	// MARK: -
 	func controllerWillChangeContent(controller: NSFetchedResultsController) {
 		self.tableView.beginUpdates()
 	}
 	func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
 		let tableView = self.tableView!
- 
 		switch type {
 		case .Insert:
 			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: UITableViewRowAnimation.Fade)
 		case .Delete:
 			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation:UITableViewRowAnimation.Fade)
 		case .Update:
-			let cell = tableView.cellForRowAtIndexPath(indexPath!)!
-			self.configureCell(cell, atIndexPath: indexPath!)
+			let indexPath = indexPath!
+			if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+				self.configureCell(cell, atIndexPath: indexPath)
+			}
 		case .Move:
 			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: UITableViewRowAnimation.Fade)
 			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation:UITableViewRowAnimation.Fade)
@@ -54,6 +60,7 @@ class SubscriptionsListViewController: UITableViewController, NSFetchedResultsCo
 	func controllerDidChangeContent(controller: NSFetchedResultsController) {
 		self.tableView.endUpdates()
 	}
+	// MARK: -
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		if segue.identifier == "show" {
 			let itemsListViewController = segue.destinationViewController as ItemsListViewController
@@ -63,6 +70,7 @@ class SubscriptionsListViewController: UITableViewController, NSFetchedResultsCo
 			itemsListViewController.streamID = subscription.id
 		}
 	}
+	// MARK: -
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return fetchedResultsController.fetchedObjects!.count
 	}
@@ -71,6 +79,7 @@ class SubscriptionsListViewController: UITableViewController, NSFetchedResultsCo
 		self.configureCell(cell, atIndexPath: indexPath)
 		return cell
 	}
+	// MARK: -
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		var fetchError: NSError?
