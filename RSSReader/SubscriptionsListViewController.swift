e9651ce7fcd22547eb5ab28a2eeca3672b0da7da
Grigory Entin <grigory.entin@gmail.com>
2015-01-08 00:00:34 +0300

    Updated for ItemsListViewController switched from .streamID to .folder.

--- RSSReader/SubscriptionsListViewController.swift
+++ RSSReader/SubscriptionsListViewController.swift
@@ -68,7 +68,7 @@ class SubscriptionsListViewController: UITableViewController, NSFetchedResultsCo
 			let indexPathForSelectedRow = self.tableView.indexPathForSelectedRow()!
 			let subscription = fetchedResultsController.fetchedObjects![indexPathForSelectedRow.row] as Subscription
 			itemsListViewController.title = subscription.title
-			itemsListViewController.streamID = subscription.id
+			itemsListViewController.folder = subscription
 		}
 	}
 	// MARK: -
