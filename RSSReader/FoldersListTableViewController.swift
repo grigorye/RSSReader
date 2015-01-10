2d3e1dfd7c8d4d79825a44bee1ed3edfce4d33a6
Grigory Entin <grigory.entin@gmail.com>
2015-01-10 23:28:24 +0300

    Updated for displaying both folders and subscriptions; switched to .rootFolder/.childContainers.

--- RSSReader/FoldersListTableViewController.swift
+++ RSSReader/FoldersListTableViewController.swift
@@ -10,77 +10,77 @@ import UIKit
 import CoreData
 
 class FoldersListTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-	lazy var fetchedResultsController: NSFetchedResultsController = {
-		let fetchRequest: NSFetchRequest = {
-			let $ = NSFetchRequest(entityName: Folder.entityName())
-			$.sortDescriptors = Folder.sortDescriptors()[0]
-			$.predicate = NSPredicate(format: "id MATCHES 'user/.*label.*'", argumentArray: [])
-			return $
-		}()
-		let $ = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.mainQueueManagedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
-		$.delegate = self
-		return $
-	}()
+	var rootFolder: Folder!
+	var childContainers: [Container] {
+		get {
+			if let rootFolder = rootFolder {
+				return rootFolder.childContainers.array as [Container]
+			}
+			else {
+				return []
+			}
+		}
+	}
 	// MARK: -
 	@IBAction func refresh(sender: AnyObject!) {
 		rssSession.updateUnreadCounts { (error: NSError?) -> Void in
-			let refreshControl = sender as UIRefreshControl
-			refreshControl.endRefreshing()
+			dispatch_async(dispatch_get_main_queue()) {
+				if nil == self.rootFolder {
+					self.rootFolder = Folder.folderWithTagSuffix(rootTagSuffix, managedObjectContext: self.mainQueueManagedObjectContext)
+				}
+				self.tableView.reloadData()
+				let refreshControl = sender as UIRefreshControl
+				refreshControl.endRefreshing()
+			}
 		}
 	}
 	// MARK: -
-	func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
-		let folder = fetchedResultsController.fetchedObjects![indexPath.row] as Folder
+	func configureCell(cell: UITableViewCell, forFolder folder: Folder) {
 		cell.textLabel?.text = folder.id.lastPathComponent
 		cell.detailTextLabel?.text = "\(folder.unreadCount)"
 	}
-	// MARK: -
-	func controllerWillChangeContent(controller: NSFetchedResultsController) {
-		self.tableView.beginUpdates()
-	}
-	func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-		let tableView = self.tableView!
- 
-		switch type {
-		case .Insert:
-			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: UITableViewRowAnimation.Fade)
-		case .Delete:
-			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation:UITableViewRowAnimation.Fade)
-		case .Update:
-			if let cell = tableView.cellForRowAtIndexPath(indexPath!) {
-				self.configureCell(cell, atIndexPath: indexPath!)
-			}
-		case .Move:
-			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: UITableViewRowAnimation.Fade)
-			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation:UITableViewRowAnimation.Fade)
-		}
-	}
-	func controllerDidChangeContent(controller: NSFetchedResultsController) {
-		self.tableView.endUpdates()
+	func configureCell(cell: UITableViewCell, forSubscription subscription: Subscription) {
+		cell.textLabel?.text = subscription.title ?? subscription.url?.lastPathComponent
+		cell.detailTextLabel?.text = "\(subscription.unreadCount)"
 	}
 	// MARK: -
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		if segue.identifier == "show" {
-			let subscriptionsListViewController = segue.destinationViewController as SubscriptionsListViewController
+		if segue.identifier == "showFolder" {
+			let foldersListTableViewController = segue.destinationViewController as FoldersListTableViewController
 			let indexPathForSelectedRow = self.tableView.indexPathForSelectedRow()!
-			let folder = fetchedResultsController.fetchedObjects![indexPathForSelectedRow.row] as Folder
-			subscriptionsListViewController.title = folder.id.lastPathComponent
-			subscriptionsListViewController.category = folder
+			let folder = childContainers[indexPathForSelectedRow.row] as Folder
+			foldersListTableViewController.title = folder.id.lastPathComponent
+			foldersListTableViewController.rootFolder = folder
+		}
+		else if segue.identifier == "showSubscription" {
+			let itemsListViewController = segue.destinationViewController as ItemsListViewController
+			let indexPathForSelectedRow = self.tableView.indexPathForSelectedRow()!
+			let subscription = childContainers[indexPathForSelectedRow.row] as Subscription
+			itemsListViewController.title = subscription.title
+			itemsListViewController.folder = subscription
 		}
 	}
 	// MARK: -
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return fetchedResultsController.fetchedObjects!.count
+		return trace("childContainers.count", childContainers.count)
 	}
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCellWithIdentifier("Folder", forIndexPath: indexPath) as UITableViewCell
-		self.configureCell(cell, atIndexPath: indexPath)
-		return cell
+		let childContainer = childContainers[indexPath.row]
+		switch childContainer {
+		case let subscription as Subscription:
+			let cell = tableView.dequeueReusableCellWithIdentifier("Subscription", forIndexPath: indexPath) as UITableViewCell
+			self.configureCell(cell, forSubscription: subscription)
+			return cell
+		case let folder as Folder:
+			let cell = tableView.dequeueReusableCellWithIdentifier("Folder", forIndexPath: indexPath) as UITableViewCell
+			self.configureCell(cell, forFolder: folder)
+			return cell
+		default:
+			assert(false, "")
+		}
 	}
 	// MARK: -
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		var fetchError: NSError?
-		fetchedResultsController.performFetch(&fetchError)
 	}
 }
