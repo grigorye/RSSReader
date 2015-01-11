4b6c8a4f0ff1da6b77dc48c8f37ccc69656b7993
Grigory Entin <grigory.entin@gmail.com>
2015-01-11 05:27:27 +0300

    Added (generated) SBConstants.swift.

---
+++ RSSReader/SBConstants.swift
@@ -0,0 +1,20 @@
+// Auto generated file from SBConstants - any changes may be lost
+
+public enum SegueIdentifier : String {
+    case show = "show"
+    case showFolder = "showFolder"
+    case showPages = "showPages"
+    case showSubscription = "showSubscription"
+    case showText = "showText"
+    case showWeb = "showWeb"
+}
+
+public enum TableViewCellReuseIdentifier : String {
+    case Folder = "Folder"
+    case Item = "Item"
+    case Subscription = "Subscription"
+}
+
+public enum ViewControllerStoryboardIdentifier : String {
+    case itemSummaryWeb = "itemSummaryWeb"
+}
