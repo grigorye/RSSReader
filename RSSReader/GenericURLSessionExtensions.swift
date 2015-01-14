0e5ef40b5e191a662c824fa432abaf09603588e3
Grigory Entin <grigory.entin@gmail.com>
2015-01-15 01:20:10 +0300

    Added NSURLSession.progressEnabledDataTaskWithRequest.

---
+++ RSSReader/GenericURLSessionExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  GenericURLSessionExtensions.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 14.01.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+extension NSURLSession {
+	func progressEnabledDataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
+		let progress = NSProgress(totalUnitCount: 1)
+		return self.dataTaskWithRequest(request) { data, response, error in
+			progress.becomeCurrentWithPendingUnitCount(1)
+			progress.resignCurrent()
+			completionHandler?(data, response, error)
+		}
+	}
+}
