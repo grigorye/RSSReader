72845bec2ed0283f2eaecb8384664bd094d40619
Grigory Entin <grigory.entin@gmail.com>
2015-01-17 21:44:03 +0300

    Added ItemTableViewCell.

---
+++ RSSReader/ItemTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemTableViewCell.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 17.01.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell : UITableViewCell {
+	@IBOutlet var titleLabel: UILabel!
+	@IBOutlet var subtitleLabel: UILabel!
+}
