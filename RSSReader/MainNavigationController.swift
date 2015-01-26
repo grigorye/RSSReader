c219b4581c19b74df3d33fe854688d2f2319b93b
Grigory Entin <grigory.entin@gmail.com>
2015-01-26 06:04:19 +0300

    Added MainNavigationController with extra left bar button item.

---
+++ RSSReader/MainNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MainNavigationController.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 26.01.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController : UINavigationController {
+	@IBOutlet var extraLeftBarButtonItem: UIBarButtonItem!
+	override func viewDidLoad() {
+		let firstViewController = self.viewControllers.first! as UIViewController
+		firstViewController.navigationItem.leftBarButtonItems = [extraLeftBarButtonItem] + (firstViewController.navigationItem.leftBarButtonItems ?? [])
+	}
+}
