a24f2aed2510b5dd4eb5c9bdf84d4fc3656566aa
Grigory Entin <grigory.entin@gmail.com>
2015-02-01 10:03:39 +0300

    Added ApplicationActivities.

---
+++ RSSReader/ApplicationActivities.swift
@@ -0,0 +1,11 @@
+//
+//  ApplicationActivities.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 01.02.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UIActivity
+
+let applicationActivities: [UIActivity] = [AddToFavoritesActivity(), MarkAllAsReadActivity(), OpenWebPageActivity()]
