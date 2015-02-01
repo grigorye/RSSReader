fb145761f66c8d7b40a072c0723829193386708c
Grigory Entin <grigory.entin@gmail.com>
2015-02-01 10:11:13 +0300

    Added MarkAllAsReadActivity.

---
+++ RSSReader/MarkAllAsReadActivity.swift
@@ -0,0 +1,43 @@
+//
+//  MarkAllAsReadActivity.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 01.02.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+
+class MarkAllAsReadActivity : TypeFilteringActivity {
+	override func performActivity() {
+		let folder = acceptedItems.last!
+		let items = (folder.valueForKey("items") as NSSet).allObjects as [Item]
+		for i in items {
+			i.markedAsRead = true
+		}
+		rssSession.markAllAsRead(folder) { error in
+			void(trace("error", error))
+		}
+	}
+	override func activityType() -> String {
+		return "\(applicationDomain).markAllAsRead"
+	}
+	override func activityTitle() -> String {
+		return NSLocalizedString("Mark All as Read", comment: "")
+	}
+	override func activityImage() -> UIImage? {
+		return UIImage(named: "AppIcon")
+	}
+	override class func activityCategory() -> UIActivityCategory {
+		return .Action
+	}
+	// MARK: -
+	typealias FilteredItem = Container
+	var itemsFilter = TypeBasedActivityItemsFilter<FilteredItem>()
+	var acceptedItems: [FilteredItem] {
+		return itemsFilter.acceptedItems
+	}
+	init() {
+		super.init(untypedItemsFilter: self.itemsFilter)
+	}
+}
