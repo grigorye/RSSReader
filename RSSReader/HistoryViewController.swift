0b5285f54309ab46adb7703c8f69c7323707fbef
Grigory Entin <grigory.entin@gmail.com>
2015-02-02 23:48:20 +0300

    Added HistoryViewController.

---
+++ RSSReader/HistoryViewController.swift
@@ -0,0 +1,115 @@
+//
+//  HistoryViewController.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 02.02.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UITableViewController
+import CoreData.NSFetchedResultsController
+
+class HistoryViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+	lazy var fetchedResultsController: NSFetchedResultsController = {
+		let fetchRequest: NSFetchRequest = {
+			let $ = NSFetchRequest(entityName: Item.entityName())
+			$.sortDescriptors = [NSSortDescriptor(key: "lastOpenedDate", ascending: false)]
+			$.predicate = NSPredicate(format: "lastOpenedDate != nil", argumentArray: [])
+			return $
+		}()
+		let $ = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.mainQueueManagedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+		$.delegate = self
+		return $
+	}()
+	// MARK: -
+	func itemForIndexPath(indexPath: NSIndexPath) -> Item {
+		return self.fetchedResultsController.fetchedObjects![indexPath.row] as Item
+	}
+	func selectedItem() -> Item {
+		return self.itemForIndexPath(self.tableView.indexPathForSelectedRow()!)
+	}
+	// MARK: -
+	func configureCell(rawCell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+		let cell = rawCell as ItemTableViewCell
+		let item = fetchedResultsController.objectAtIndexPath(indexPath) as Item
+		if let titleLabel = cell.titleLabel {
+			titleLabel.text = item.title ?? item.id.lastPathComponent
+		}
+	}
+	// MARK: -
+	func controllerWillChangeContent(controller: NSFetchedResultsController) {
+		self.tableView.beginUpdates()
+	}
+	let rowAnimation = UITableViewRowAnimation.None
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+		switch type {
+		case .Insert:
+			tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: rowAnimation)
+		case .Delete:
+			tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: rowAnimation)
+		default:
+			abort()
+		}
+	}
+	func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+		let tableView = self.tableView!
+		switch type {
+		case .Insert:
+			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: rowAnimation)
+		case .Delete:
+			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: rowAnimation)
+		case .Update:
+			if let cell = tableView.cellForRowAtIndexPath(indexPath!) {
+				self.configureCell(cell, atIndexPath: indexPath!)
+			}
+		case .Move:
+			tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: rowAnimation)
+			tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: rowAnimation)
+		}
+	}
+	func controllerDidChangeContent(controller: NSFetchedResultsController) {
+		(_1 ? UIView.performWithoutAnimation : invoke) {
+			self.tableView.endUpdates()
+		}
+	}
+	// MARK: -
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return fetchedResultsController.sections!.count
+	}
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return (fetchedResultsController.sections![section] as NSFetchedResultsSectionInfo).numberOfObjects
+	}
+	override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return (fetchedResultsController.sections![section] as NSFetchedResultsSectionInfo).name
+	}
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier(TableViewCellReuseIdentifier.HistoryItem.rawValue, forIndexPath: indexPath) as UITableViewCell
+		self.configureCell(cell, atIndexPath: indexPath)
+		return cell
+	}
+	// MARK: -
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		switch SegueIdentifier(rawValue: segue.identifier!)! {
+		case .showPages:
+			let pageViewController = segue.destinationViewController as UIPageViewController
+			let itemsPageViewControllerDataSource: ItemsPageViewControllerDataSource = {
+				let $ = pageViewController.dataSource as ItemsPageViewControllerDataSource
+				$.items = self.fetchedResultsController.fetchedObjects as [Item]
+				return $
+			}()
+			let initialViewController = itemsPageViewControllerDataSource.viewControllerForItem(self.selectedItem(), storyboard: pageViewController.storyboard!)
+			if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+				pageViewController.edgesForExtendedLayout = .None
+			}
+			pageViewController.setViewControllers([initialViewController], direction: .Forward, animated: false, completion: nil)
+		default:
+			abort()
+		}
+	}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		var fetchError: NSError?
+		fetchedResultsController.performFetch(&fetchError)
+		assert(nil == fetchError, "")
+    }
+}
