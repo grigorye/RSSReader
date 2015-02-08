2a4d861fea29dc1c63b94a3bd0d2d0ea2e7c4c8d
Grigory Entin <grigory.entin@gmail.com>
2015-02-08 23:52:05 +0300

    Added ItemsPageViewController capable of state restoration.

---
+++ RSSReader/ItemsPageViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ItemsPageViewController.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 08.02.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UIPageViewController
+
+class ItemsPageViewController : UIPageViewController {
+	var blocksDelayedTillViewWillAppear = [Handler]()
+	// MARK: - State Preservation and Restoration
+	private enum Restorable: String {
+		case pageViewControllers = "pageViewControllers"
+	}
+	override func decodeRestorableStateWithCoder(coder: NSCoder) {
+		super.decodeRestorableStateWithCoder(coder)
+		let dataSource = self.dataSource as ItemsPageViewControllerDataSource
+		dataSource.decodeRestorableStateWithCoder(coder)
+		if _1 {
+			let viewControllers = coder.decodeObjectForKey(Restorable.pageViewControllers.rawValue) as [UIViewController]
+			blocksDelayedTillViewWillAppear += [{
+				self.setViewControllers(viewControllers, direction: .Forward, animated: false) { completed in
+					trace("completed", completed)
+					return
+				}
+			}]
+		}
+	}
+	override func encodeRestorableStateWithCoder(coder: NSCoder) {
+		super.encodeRestorableStateWithCoder(coder)
+		let dataSource = self.dataSource as ItemsPageViewControllerDataSource
+		dataSource.encodeRestorableStateWithCoder(coder)
+		if _1 {
+			coder.encodeObject(self.viewControllers, forKey: Restorable.pageViewControllers.rawValue)
+		}
+	}
+	// MARK: -
+	override func viewWillAppear(animated: Bool) {
+		for i in blocksDelayedTillViewWillAppear { i() }
+		blocksDelayedTillViewWillAppear = []
+		super.viewWillAppear(animated)
+	}
+}
+
