2920ddd1d9f8b72f432bed5b1d8d22ea4bdb9ef6
Grigory Entin <grigory.entin@gmail.com>
2015-02-12 22:50:14 +0300

    Added cocoa pods, including AFMInfoBanner.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFMInfoBanner
+#define COCOAPODS_POD_AVAILABLE_AFMInfoBanner
+#define COCOAPODS_VERSION_MAJOR_AFMInfoBanner 1
+#define COCOAPODS_VERSION_MINOR_AFMInfoBanner 0
+#define COCOAPODS_VERSION_PATCH_AFMInfoBanner 0
+
+// CVKHierarchySearcher
+#define COCOAPODS_POD_AVAILABLE_CVKHierarchySearcher
+#define COCOAPODS_VERSION_MAJOR_CVKHierarchySearcher 0
+#define COCOAPODS_VERSION_MINOR_CVKHierarchySearcher 1
+#define COCOAPODS_VERSION_PATCH_CVKHierarchySearcher 0
+
