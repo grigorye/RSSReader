17a52c0733503fb37644dc39d397af1ee9e7f6e1
Grigory Entin <grigory.entin@gmail.com>
2015-03-03 06:14:49 +0300

    Added UITableView+SnappingHeaderToTop.

---
+++ RSSReader/UITableView+SnappingHeaderToTop.swift
@@ -0,0 +1,19 @@
+//
+//  UITableView+SnappingHeaderToTop.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 03/03/15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UITableView
+
+extension UITableView {
+	func snapHeaderToTop(#animated: Bool) {
+		let insetAwareTableHeaderViewFrame = CGRectOffset(tableHeaderView!.frame, 0, -contentInset.top)
+		if contentOffset.y < CGRectGetMaxY(insetAwareTableHeaderViewFrame) {
+			let adjustedContentOffsetY = (contentOffset.y < CGRectGetMidY(insetAwareTableHeaderViewFrame) ? CGRectGetMinY : CGRectGetMaxY)(insetAwareTableHeaderViewFrame);
+			setContentOffset(CGPoint(x: contentOffset.x, y: adjustedContentOffsetY), animated: animated)
+		}
+	}
+}
