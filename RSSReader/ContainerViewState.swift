238300e00f474311d666bbb99aeeec176310c9c7
Grigory Entin <grigory.entin@gmail.com>
2015-03-16 00:55:02 +0300

    Added ContainerViewState.

---
+++ RSSReader/ContainerViewState.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerViewState.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 16/03/15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+import CoreData.NSManagedObject
+
+class ContainerViewState: NSManagedObject {
+    @NSManaged var continuation: String?
+    @NSManaged var loadError: NSError?
+    @NSManaged var loadDate: NSDate?
+    @NSManaged var loadCompleted: Bool
+    @NSManaged var lastLoadedItem: Item?
+    @NSManaged var container: Container?
+}
