e42e7c6b7541a2641288ef7b9dbc6a4d9858b64f
Grigory Entin <grigory.entin@gmail.com>
2015-03-22 18:54:03 +0300

    Moved in exapand, action IB actions; added [un]markAsFavorite IB actions/bar button items.

--- RSSReader/ItemSummaryWebViewController.swift
+++ RSSReader/ItemSummaryWebViewController.swift
@@ -13,6 +13,9 @@ let markAsReadTimeInterval = NSTimeInterval(2)
 
 class ItemSummaryWebViewController: UIViewController {
 	@IBOutlet var webView: UIWebView!
+	var savedRightBarButtonItems: [UIBarButtonItem]!
+	@IBOutlet var markAsFavoriteBarButtonItem: UIBarButtonItem!
+	@IBOutlet var unmarkAsFavoriteBarButtonItem: UIBarButtonItem!
 	var item: Item!
 	var markAsReadTimer: NSTimer?
 	func markAsRead() {
@@ -62,19 +65,84 @@ class ItemSummaryWebViewController: UIViewController {
 		}
 	}
 	// MARK: -
+	@IBAction func markAsFavorite(sender: AnyObject?, event: UIEvent?) {
+		item.markedAsFavorite = true
+		rssSession!.uploadTag(canonicalFavoriteTag, mark: true, forItem: item, completionHandler: { uploadFavoritesStateError in
+			if let uploadFavoritesStateError = uploadFavoritesStateError {
+				trace("uploadFavoritesStateError", uploadFavoritesStateError)
+				presentErrorMessage(NSLocalizedString("Failed to mark as favorite.", comment: ""))
+			}
+		})
+	}
+	@IBAction func unmarkAsFavorite(sender: AnyObject?, event: UIEvent?) {
+		item.markedAsFavorite = false
+		rssSession!.uploadTag(canonicalFavoriteTag, mark: false, forItem: item, completionHandler: { uploadFavoritesStateError in
+			if let uploadFavoritesStateError = uploadFavoritesStateError {
+				trace("uploadFavoritesStateError", uploadFavoritesStateError)
+				presentErrorMessage(NSLocalizedString("Failed to unmark as favorite.", comment: ""))
+			}
+		})
+	}
+	@IBAction func action(sender: AnyObject?, event: UIEvent?) {
+		let activityViewController: UIViewController = {
+			let item = self.item
+			let href = item.canonical!.first!["href"]!
+			let url = NSURL(string: href)!
+			let activityItems = [url, item]
+			return UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+		}()
+		self.presentViewController(activityViewController, animated: true, completion: nil)
+	}
+	@IBAction func expand(sender: AnyObject?, event: UIEvent?) {
+		let item = self.item
+		let href = item.canonical!.first!["href"]!
+		let url = NSURL(string: href)!
+		let dataTask = progressEnabledURLSessionTaskGenerator.textTaskForHTTPRequest(NSURLRequest(URL: url)) { text, error in
+			dispatch_async(dispatch_get_main_queue()) {
+				if let error = error {
+					void(trace("error", error))
+					presentErrorMessage(NSLocalizedString("Failed to expand.", comment: ""))
+				}
+				else {
+					let readability = DZReadability(URL: url, rawDocumentContent: text, options: nil) { sender, content, error in
+						if let error = error {
+							void(trace("error", error))
+							presentErrorMessage(NSLocalizedString("Unable to expand", comment: ""))
+						}
+						else {
+							self.loadHTMLString(content, ignoringExisting: true)
+						}
+					}
+					readability.start()
+				}
+			}
+		}
+		dataTask.resume()
+	}
+	// MARK: -
 	var blocksScheduledForViewWillAppear = [Handler]()
+	var itemMarkedAsReadKVOBinding: KVOBinding?
 	// MARK: -
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.savedRightBarButtonItems = self.navigationItem.rightBarButtonItems! as! [UIBarButtonItem]
 		blocksScheduledForViewWillAppear += [{
 			let item = self.item
 			self.loadHTMLString(item.summary!, ignoringExisting: false)
 		}]
 	}
 	// MARK: -
+	var viewDidDisappearRetainedObjects = [AnyObject]()
 	override func viewWillAppear(animated: Bool) {
 		for i in blocksScheduledForViewWillAppear { i() }
 		blocksScheduledForViewWillAppear = []
+		viewDidDisappearRetainedObjects += [KVOBinding(object: self, keyPath: "item.markedAsFavorite", options: .Initial) { [unowned self] change in
+			let excludedBarButtonItem = self.item.markedAsFavorite ? self.markAsFavoriteBarButtonItem : self.unmarkAsFavoriteBarButtonItem
+			let rightBarButtonItems = filter(self.savedRightBarButtonItems) {
+				return $0 != excludedBarButtonItem
+			}
+			self.navigationItem.rightBarButtonItems = trace("rightBarButtonItems", rightBarButtonItems)
+		}]
 		super.viewWillAppear(animated)
 	}
 	override func viewDidAppear(animated: Bool) {
@@ -85,6 +153,10 @@ class ItemSummaryWebViewController: UIViewController {
 	override func viewWillDisappear(animated: Bool) {
 		super.viewWillDisappear(animated)
 		self.markAsReadTimer?.invalidate()
+	}
+	override func viewDidDisappear(animated: Bool) {
+		viewDidDisappearRetainedObjects = []
+		super.viewDidDisappear(animated)
 	}
 	// MARK: - State Preservation and Restoration
 	enum Restorable: String {
