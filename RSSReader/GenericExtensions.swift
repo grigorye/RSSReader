3deaa1f4d7b8b58e7a3e8e288248c2dfd3821466
Grigory Entin <grigory.entin@gmail.com>
2015-04-05 13:54:34 +0300

    Switched from [no]trace to $().$(); added traceToNSLogEnabled.

--- RSSReader/GenericExtensions.swift
+++ RSSReader/GenericExtensions.swift
@@ -14,23 +14,53 @@ import Crashlytics
 #endif
 #endif
 
+private let traceToNSLogEnabled = true
+
 var traceEnabled: Bool {
 	return defaults.traceEnabled
+}
+var traceLabelsEnabled: Bool {
+	return defaults.traceLabelsEnabled
 }
 
 func description<T>(value: T) -> String {
 	return "\(value)"
 }
 
-func traceString(label: String, string: String, file: NSString = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
+struct SourceLocation {
+	let file: String
+	let line: Int
+	let column: Int
+	let function: String
+	init(file: String = __FILE__, line: Int = __LINE__, column: Int = __COLUMN__, function: String = __FUNCTION__) {
+		self.file = file
+		self.line = line
+		self.column = column
+		self.function = function
+	}
+}
+
+func labelFromLocations(firstLocation: SourceLocation, lastLocation: SourceLocation) -> String {
+	let fileName = firstLocation.file.lastPathComponent
+	let file = NSBundle.mainBundle().pathForResource(fileName.stringByDeletingPathExtension, ofType: fileName.pathExtension)!
+	let text = NSString(contentsOfFile: file, encoding: NSUTF8StringEncoding, error: nil)
+	let lines = text?.componentsSeparatedByString("\n") as! [NSString]
+	let range = NSRange(location: firstLocation.column - 1, length: lastLocation.column - firstLocation.column-3)
+	return lines[firstLocation.line - 1].substringWithRange(range)
+}
+
+func traceString(string: String, location: SourceLocation, lastLocation: SourceLocation) {
 	if traceEnabled {
-		let message = "\(file.lastPathComponent), \(function).\(line): \(label): \(string)"
+		let labelSuffix = !traceLabelsEnabled ? "[\(location.column)-\(lastLocation.column)]" : {
+			return ": \(labelFromLocations(location, lastLocation))"
+		}()
+		let message = "\(location.file.lastPathComponent), \(location.function).\(location.line)\(labelSuffix): \(string)"
 #if ANALYTICS_ENABLED
 #if CRASHLYTICS_ENABLED
 		CLSLogv("%@", getVaList([message]))
 #endif
 #endif
-		if _1 {
+		if traceToNSLogEnabled {
 			NSLog("%@", message)
 		}
 		else {
@@ -39,12 +69,27 @@ func traceString(label: String, string: String, file: NSString = __FILE__, line:
 	}
 }
 
-func trace<T>(label: String, value: T, file: NSString = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) -> T {
-	traceString(label, description(value), file: file, line: line, function: function)
-	return value
+struct Traceable<T> {
+	let value: T
+	let location: SourceLocation
+	init(value: T, location: SourceLocation = SourceLocation(file: __FILE__, line: __LINE__, column: __COLUMN__, function: __FUNCTION__)) {
+		self.value = value
+		self.location = location
+	}
+	func $(_ level: Int = 1, file: String = __FILE__, line: Int = __LINE__, column: Int = __COLUMN__, function: String = __FUNCTION__) -> T {
+		if 0 != level {
+			trace(value, self.location, SourceLocation(file: file, line: line, column: column, function: function))
+		}
+		return value
+	}
 }
 
-func notrace<T>(label: String, value: T, file: NSString = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) -> T {
+func $<T>(v: T, file: String = __FILE__, line: Int = __LINE__, column: Int = __COLUMN__, function: String = __FUNCTION__) -> Traceable<T> {
+	return Traceable(value: v, location: SourceLocation(file: file, line: line, column: column, function: function))
+}
+
+func trace<T>(value: T, startLocation: SourceLocation, endLocation: SourceLocation) -> T {
+	traceString(description(value), startLocation, endLocation)
 	return value
 }
 
@@ -67,7 +112,7 @@ func URLQuerySuffixFromComponents(components: [String]) -> String {
 
 func filterObjectsByType<T>(objects: [AnyObject]) -> [T] {
 	let filteredObjects = objects.reduce([T]()) {
-		if let x = trace("$1", $1) as? T {
+		if let x = $($1).$() as? T {
 			return $0 + [x]
 		}
 		else {
