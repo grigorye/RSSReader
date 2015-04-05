e006e442a2eb238ae0d29c002a26d2cb3bdab890
Grigory Entin <grigory.entin@gmail.com>
2015-04-06 00:51:31 +0300

    Switched to $().$().

--- RSSReader/AddToFavoritesActivity.swift
+++ RSSReader/AddToFavoritesActivity.swift
@@ -14,7 +14,7 @@ class AddToFavoritesActivity : TypeFilteringActivity  {
 		item.markedAsFavorite = true
 		rssSession!.uploadTag(canonicalFavoriteTag, mark: true, forItem: item, completionHandler: { uploadFavoritesStateError in
 			if let uploadFavoritesStateError = uploadFavoritesStateError {
-				trace("uploadFavoritesStateError", uploadFavoritesStateError)
+				$(uploadFavoritesStateError).$()
 				presentErrorMessage(NSLocalizedString("Failed to mark as favorite.", comment: ""))
 			}
 		})
