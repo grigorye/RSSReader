e48e32e5b26548dc7930ea86a47bb9c36c30595c
Grigory Entin <grigory.entin@gmail.com>
2015-04-19 00:22:24 +0300

    Added NSBundleExtensions.

---
+++ RSSReader/NSBundleExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSBundleExtensions.h
+//  RSSReader
+//
+//  Created by Grigory Entin on 17.04.15.
+//  Copyright (c) 2015 Grigory Entin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (Backtrace)
+
++ (NSBundle *)bundleOnStack;
+
+@end
