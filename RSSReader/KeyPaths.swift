912c9708dd6983b107018222877eaf5df3f44fd0
Grigory Entin <grigory.entin@gmail.com>
2015-04-20 07:30:46 +0300

    Switched to proxy.valueClass; switched to object_setClassAndRetain; added returned value workaround for Stirng!; moved out ObjectAndKeyPath/prefix operator.

--- RSSReader/KeyPaths.swift
+++ RSSReader/KeyPaths.swift
@@ -10,55 +10,34 @@ import Foundation
 
 // MARK: -
 
-private func recordKeyPath<T: NSObject, V>(fakeReturnValue: NSObject?, x: T, @noescape recorder: (T) -> V) -> String {
+private func recordKeyPath<T: NSObject, V>(valueClass: AnyClass!, x: T, @noescape recorder: (T) -> V) -> String {
 	let proxy = KeyPathRecordingProxy.alloc()
-	proxy.fakeReturnValue = fakeReturnValue
-	let savedObjectClass: AnyClass! = object_getClass(x)
-	proxy.realObjectClass = savedObjectClass
+	proxy.valueClass = valueClass
 	objc_setAssociatedObject(x, keyPathRecorderProxyAssociation, proxy, UInt(OBJC_ASSOCIATION_ASSIGN))
-	object_setClass(x, KeyPathRecordingProxy.self)
+	let objectClass: AnyClass = object_setClassAndRetain(x, KeyPathRecordingProxy.self)
 	recorder(x)
-	object_setClass(x, savedObjectClass)
+	object_setClassAndRetain(x, objectClass)
+	objc_setAssociatedObject(x, keyPathRecorderProxyAssociation, nil, UInt(OBJC_ASSOCIATION_ASSIGN))
 	let keyPath = join(".", proxy.keyPathComponents! as! [String])
 	return $(keyPath).$()
 }
 
 /// Specialization for the (Array-bound) code emitted by the compiler for any function call returning an Array.
 public func instanceKeyPath<T: NSObject, V: AnyObject>(x: T, @noescape recorder: (T) -> [V]) -> String {
-	return recordKeyPath([V](), x, recorder)
+	return recordKeyPath(NSArray.self, x, recorder)
 }
 
 /// Specialization for the (String-bound) code emitted by the compiler for any function call returning a String.
 public func instanceKeyPath<T: NSObject>(x: T, @noescape recorder: (T) -> String) -> String {
-	return recordKeyPath("", x, recorder)
+	return recordKeyPath(NSString.self, x, recorder)
 }
 
-/// 
+/// Specialization for the (String-bound) code emitted by the compiler for any function call returning an optional String.
+public func instanceKeyPath<T: NSObject>(x: T, @noescape recorder: (T) -> Optional<String>) -> String {
+	return recordKeyPath(NSString.self, x, {recorder($0)!})
+}
+
+/// Generic implementation.
 public func instanceKeyPath<T: NSObject, V>(x: T, @noescape recorder: (T) -> V) -> String {
 	return recordKeyPath(nil, x, recorder)
-}
-
-// MARK: -
-
-public struct ObjectAndKeyPath {
-	public let object: NSObject
-	public let keyPath: String
-	init(objectAndKeyPath: ObjectAndKeyPath) {
-		self.object = objectAndKeyPath.object
-		self.keyPath = objectAndKeyPath.keyPath
-	}
-	init(_ object: NSObject, _ keyPath: String) {
-		self.object = object
-		self.keyPath = keyPath
-	}
-}
-
-// MARK: - 
-
-prefix operator • {}
-
-public prefix func •<T: NSObject, V>(x: T) -> ((T) -> V) -> ObjectAndKeyPath {
-	return { (@noescape recorder: (T) -> V) in
-		return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
-	}
 }
