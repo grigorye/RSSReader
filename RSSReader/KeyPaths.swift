d309733b26f22405ef012e904a49c0c1ba87c100
Grigory Entin <grigory.entin@gmail.com>
2015-04-21 08:12:47 +0300

    Switched to T? as the target type; switched to directly messaged proxy.

--- RSSReader/KeyPaths.swift
+++ RSSReader/KeyPaths.swift
@@ -10,34 +10,31 @@ import Foundation
 
 // MARK: -
 
-private func recordKeyPath<T: NSObject, V>(valueClass: AnyClass!, x: T, @noescape recorder: (T) -> V) -> String {
+private func recordKeyPath<T: NSObject, V>(valueClass: AnyClass!, x: T?, @noescape recorder: (T) -> V) -> String {
 	let proxy = KeyPathRecordingProxy.alloc()
 	proxy.valueClass = valueClass
-	objc_setAssociatedObject(x, keyPathRecorderProxyAssociation, proxy, UInt(OBJC_ASSOCIATION_ASSIGN))
-	let objectClass: AnyClass = object_setClassAndRetain(x, KeyPathRecordingProxy.self)
-	recorder(x)
-	object_setClassAndRetain(x, objectClass)
-	objc_setAssociatedObject(x, keyPathRecorderProxyAssociation, nil, UInt(OBJC_ASSOCIATION_ASSIGN))
+	let proxifiedX = proxy as! T
+	recorder(proxifiedX)
 	let keyPath = join(".", proxy.keyPathComponents! as! [String])
 	return $(keyPath).$()
 }
 
 /// Specialization for the (Array-bound) code emitted by the compiler for any function call returning an Array.
-public func instanceKeyPath<T: NSObject, V: AnyObject>(x: T, @noescape recorder: (T) -> [V]) -> String {
+public func instanceKeyPath<T: NSObject, V: AnyObject>(x: T?, @noescape recorder: (T) -> [V]) -> String {
 	return recordKeyPath(NSArray.self, x, recorder)
 }
 
 /// Specialization for the (String-bound) code emitted by the compiler for any function call returning a String.
-public func instanceKeyPath<T: NSObject>(x: T, @noescape recorder: (T) -> String) -> String {
+public func instanceKeyPath<T: NSObject>(x: T?, @noescape recorder: (T) -> String) -> String {
 	return recordKeyPath(NSString.self, x, recorder)
 }
 
 /// Specialization for the (String-bound) code emitted by the compiler for any function call returning an optional String.
-public func instanceKeyPath<T: NSObject>(x: T, @noescape recorder: (T) -> Optional<String>) -> String {
+public func instanceKeyPath<T: NSObject>(x: T?, @noescape recorder: (T) -> Optional<String>) -> String {
 	return recordKeyPath(NSString.self, x, {recorder($0)!})
 }
 
 /// Generic implementation.
-public func instanceKeyPath<T: NSObject, V>(x: T, @noescape recorder: (T) -> V) -> String {
+public func instanceKeyPath<T: NSObject, V>(x: T?, @noescape recorder: (T) -> V) -> String {
 	return recordKeyPath(nil, x, recorder)
 }
