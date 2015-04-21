6dd32a1c5e7d53ec1b53874c7c8e315fb12c4313
Grigory Entin <grigory.entin@gmail.com>
2015-04-21 08:47:08 +0300

    Added tests for set/dictionary/optional target.

--- RSSReader/KeyPathsTests-GeneratorFunction.swift
+++ RSSReader/KeyPathsTests-GeneratorFunction.swift
@@ -18,9 +18,14 @@ extension KeyPathsTests {
 		XCTAssertEqual(xxx(self){$0.array}.keyPath, "array")
 		XCTAssertEqual(xxx(self){$0.optionalArray}.keyPath, "optionalArray")
 		XCTAssertEqual(xxx(self){$0.computedArray}.keyPath, "computedArray")
+		XCTAssertEqual(xxx(self){$0.set}.keyPath, "set")
+		XCTAssertEqual(xxx(self){$0.optionalSet}.keyPath, "optionalSet")
+		XCTAssertEqual(xxx(self){$0.dictionary}.keyPath, "dictionary")
+		XCTAssertEqual(xxx(self){$0.optionalDictionary}.keyPath, "optionalDictionary")
 		XCTAssertEqual(xxx(self){$0.optionalObject.string}.keyPath, "optionalObject.string")
 		XCTAssertEqual(xxx(self){$0.optionalComputedObject.string}.keyPath, "optionalComputedObject.string")
 		XCTAssertEqual(xxx(self){$0.optionalComputedObject.optionalAnotherObject.computedLength}.keyPath, "optionalComputedObject.optionalAnotherObject.computedLength")
+		XCTAssertEqual(xxx(self.optionalObject){$0.string}.keyPath, "string")
 		XCTAssertEqual(keyPathRecordingProxyLiveCount, oldKeyPathRecordingProxyLiveCount)
 	}
 }
