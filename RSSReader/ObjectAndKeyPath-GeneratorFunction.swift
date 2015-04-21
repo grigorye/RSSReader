70134865839609d9d65c1d6b5ff46a314abd92fa
Grigory Entin <grigory.entin@gmail.com>
2015-04-21 08:39:25 +0300

    Switched object in object and key path operator to T!.

--- RSSReader/ObjectAndKeyPath-GeneratorFunction.swift
+++ RSSReader/ObjectAndKeyPath-GeneratorFunction.swift
@@ -6,15 +6,17 @@
 //  Copyright (c) 2015 Grigory Entin. All rights reserved.
 //
 
-public func xxx<T: NSObject, V: AnyObject>(x: T, @noescape recorder: (T) -> [V]) -> ObjectAndKeyPath {
+import Foundation
+
+public func xxx<T: NSObject, V: AnyObject>(x: T!, @noescape recorder: (T!) -> [V]) -> ObjectAndKeyPath {
 	return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 }
-public func xxx<T: NSObject>(x: T, @noescape recorder: (T) -> String) -> ObjectAndKeyPath {
+public func xxx<T: NSObject>(x: T!, @noescape recorder: (T!) -> String) -> ObjectAndKeyPath {
 	return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 }
-public func xxx<T: NSObject>(x: T, @noescape recorder: (T) -> Optional<String>) -> ObjectAndKeyPath {
+public func xxx<T: NSObject>(x: T!, @noescape recorder: (T!) -> Optional<String>) -> ObjectAndKeyPath {
 	return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 }
-public func xxx<T: NSObject, V>(x: T, @noescape recorder: (T) -> V) -> ObjectAndKeyPath {
+public func xxx<T: NSObject, V>(x: T!, @noescape recorder: (T!) -> V) -> ObjectAndKeyPath {
 	return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 }
--- RSSReader/ObjectAndKeyPath-PrefixOperator.swift
+++ RSSReader/ObjectAndKeyPath-PrefixOperator.swift
@@ -10,18 +10,18 @@ import Foundation
 
 prefix operator •• {}
 
-public prefix func ••<T: NSObject, V: AnyObject>(x: T) -> ((T) -> [V]) -> ObjectAndKeyPath {
-	return { (@noescape recorder: (T) -> [V]) in
+public prefix func ••<T: NSObject, V: AnyObject>(x: T) -> ((T!) -> [V]) -> ObjectAndKeyPath {
+	return { (@noescape recorder: (T!) -> [V]) in
 		return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 	}
 }
-public prefix func ••<T: NSObject, V>(x: T) -> ((T) -> String) -> ObjectAndKeyPath {
-	return { (@noescape recorder: (T) -> String) in
+public prefix func ••<T: NSObject, V>(x: T) -> ((T!) -> String) -> ObjectAndKeyPath {
+	return { (@noescape recorder: (T!) -> String) in
 		return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 	}
 }
-public prefix func ••<T: NSObject, V>(x: T) -> ((T) -> V) -> ObjectAndKeyPath {
-	return { (@noescape recorder: (T) -> V) in
+public prefix func ••<T: NSObject, V>(x: T) -> ((T!) -> V) -> ObjectAndKeyPath {
+	return { (@noescape recorder: (T!) -> V) in
 		return ObjectAndKeyPath(x, instanceKeyPath(x, recorder))
 	}
 }
