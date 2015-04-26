a57cea9b622e30afbc39a12b9cc034456bc08fbe
Grigory Entin <grigory.entin@gmail.com>
2015-04-27 02:06:49 +0300

    Removed bridged variants of instanceKeyPath; switched to external recordKeyPath.

--- RSSReader/KeyPaths.swift
+++ RSSReader/KeyPaths.swift
@@ -8,33 +8,9 @@
 
 import Foundation
 
-// MARK: -
-
-private func recordKeyPath<T: NSObject, V>(valueClass: AnyClass!, x: T?, @noescape recorder: (T) -> V) -> String {
-	let proxy = KeyPathRecordingProxy.alloc()
-	proxy.valueClass = valueClass
-	let proxifiedX = proxy as! T
-	recorder(proxifiedX)
-	let keyPath = join(".", proxy.keyPathComponents! as! [String])
+public func instanceKeyPath<T: NSObject>(x: T?, recorder: ((T!) -> Void)!) -> String {
+	let keyPath = recordKeyPath { x in
+		recorder(x as! T!)
+	}
 	return $(keyPath).$()
-}
-
-/// Specialization for the (Array-bound) code emitted by the compiler for any function call returning an Array.
-public func instanceKeyPath<T: NSObject, V: AnyObject>(x: T?, @noescape recorder: (T) -> [V]) -> String {
-	return recordKeyPath(NSArray.self, x, recorder)
-}
-
-/// Specialization for the (String-bound) code emitted by the compiler for any function call returning a String.
-public func instanceKeyPath<T: NSObject>(x: T?, @noescape recorder: (T) -> String) -> String {
-	return recordKeyPath(NSString.self, x, recorder)
-}
-
-/// Specialization for the (String-bound) code emitted by the compiler for any function call returning an optional String.
-public func instanceKeyPath<T: NSObject>(x: T?, @noescape recorder: (T) -> Optional<String>) -> String {
-	return recordKeyPath(NSString.self, x, {recorder($0)!})
-}
-
-/// Generic implementation.
-public func instanceKeyPath<T: NSObject, V>(x: T?, @noescape recorder: (T) -> V) -> String {
-	return recordKeyPath(nil, x, recorder)
 }
