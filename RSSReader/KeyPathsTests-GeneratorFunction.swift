e4dd4775a16ec8fd6562082b88f8fc08ced9cdb9
Grigory Entin <grigory.entin@gmail.com>
2015-04-28 02:54:05 +0300

    Updated for new name used for objectAndKeyPath; updated for setOfStrings.

--- RSSReader/KeyPathsTests-GeneratorFunction.swift
+++ RSSReader/KeyPathsTests-GeneratorFunction.swift
@@ -13,19 +13,20 @@ import Foundation
 extension KeyPathsTests {
 	func testGeneratorFunction() {
 		let oldKeyPathRecordingProxyLiveCount = keyPathRecordingProxyLiveCount
-		XCTAssertEqual(xxx(self){$0.string}.keyPath, "string")
-		XCTAssertEqual(xxx(self){$0.optionalString}.keyPath, "optionalString")
-		XCTAssertEqual(xxx(self){$0.array}.keyPath, "array")
-		XCTAssertEqual(xxx(self){$0.optionalArray}.keyPath, "optionalArray")
-		XCTAssertEqual(xxx(self){$0.computedArray}.keyPath, "computedArray")
-		XCTAssertEqual(xxx(self){$0.set}.keyPath, "set")
-		XCTAssertEqual(xxx(self){$0.optionalSet}.keyPath, "optionalSet")
-		XCTAssertEqual(xxx(self){$0.dictionary}.keyPath, "dictionary")
-		XCTAssertEqual(xxx(self){$0.optionalDictionary}.keyPath, "optionalDictionary")
-		XCTAssertEqual(xxx(self){$0.optionalObject.string}.keyPath, "optionalObject.string")
-		XCTAssertEqual(xxx(self){$0.optionalComputedObject.string}.keyPath, "optionalComputedObject.string")
-		XCTAssertEqual(xxx(self){$0.optionalComputedObject.optionalAnotherObject.computedLength}.keyPath, "optionalComputedObject.optionalAnotherObject.computedLength")
-		XCTAssertEqual(xxx(self.optionalObject){$0.string}.keyPath, "string")
+		XCTAssertEqual(objectAndKeyPath(self){$0.string}.keyPath, "string")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalString}.keyPath, "optionalString")
+		XCTAssertEqual(objectAndKeyPath(self){$0.array}.keyPath, "array")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalArray}.keyPath, "optionalArray")
+		XCTAssertEqual(objectAndKeyPath(self){$0.computedArray}.keyPath, "computedArray")
+		XCTAssertEqual(objectAndKeyPath(self){$0.setOfStrings}.keyPath, "setOfStrings")
+		XCTAssertEqual(objectAndKeyPath(self){$0.set}.keyPath, "set")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalSet}.keyPath, "optionalSet")
+		XCTAssertEqual(objectAndKeyPath(self){$0.dictionary}.keyPath, "dictionary")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalDictionary}.keyPath, "optionalDictionary")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalObject.string}.keyPath, "optionalObject.string")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalComputedObject.string}.keyPath, "optionalComputedObject.string")
+		XCTAssertEqual(objectAndKeyPath(self){$0.optionalComputedObject.optionalAnotherObject.computedLength}.keyPath, "optionalComputedObject.optionalAnotherObject.computedLength")
+		XCTAssertEqual(objectAndKeyPath(self.optionalObject){$0.string}.keyPath, "string")
 		XCTAssertEqual(keyPathRecordingProxyLiveCount, oldKeyPathRecordingProxyLiveCount)
 	}
 }
