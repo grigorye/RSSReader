42c3eb49304a55f118b0317334fdc6852f1b3ce2
Grigory Entin <grigory.entin@gmail.com>
2015-06-22 10:00:36 +0300

    Added KeyPathRecordingProxySwift.

---
+++ RSSReader/KeyPathRecordingProxySwift.swift
@@ -0,0 +1,12 @@
+//
+//  KeyPathRecordingProxySwift.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 20.06.15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+@objc(KeyPathRecordingProxySwift) public class KeyPathRecordingProxySwift : KeyPathRecordingProxy {
+}
