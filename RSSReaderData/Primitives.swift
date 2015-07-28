a8bcc939ae964464c4fd806157d7cdfb6f3141f6
Grigory Entin <grigory.entin@gmail.com>
2015-07-28 17:27:20 +0300

    Segregated Primitives.swift.

---
+++ RSSReaderData/Primitives.swift
@@ -0,0 +1,10 @@
+//
+//  Primitives.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 18.07.15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+var _1 = true
+var _0 = false
