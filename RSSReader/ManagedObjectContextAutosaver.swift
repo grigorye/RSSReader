3adf746e9852d7d51ae310140f6b758bc2c05982
Grigory Entin <grigory.entin@gmail.com>
2015-07-28 17:27:51 +0300

    Segregated ManagedObjectContextAutosaver.swift.

---
+++ RSSReader/ManagedObjectContextAutosaver.swift
@@ -0,0 +1,36 @@
+//
+//  ManagedObjectContextAutosaver.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 13.07.15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ManagedObjectContextAutosaver: NSObject {
+	let notificationCenter = NSNotificationCenter.defaultCenter()
+	let observer: AnyObject
+	deinit {
+		notificationCenter.removeObserver(observer)
+	}
+	init(managedObjectContext: NSManagedObjectContext, queue: NSOperationQueue?) {
+		observer = notificationCenter.addObserverForName(NSManagedObjectContextObjectsDidChangeNotification, object: managedObjectContext, queue: queue, usingBlock: { notification in
+			$(notification).$()
+			managedObjectContext.performBlock {
+				$(managedObjectContext).$()
+				if let updatedObjects = notification.userInfo?[NSUpdatedObjectsKey] as! Set<NSManagedObject>? {
+					for updatedObject in updatedObjects {
+						$($(updatedObject).$().changedValues()).$()
+					}
+				}
+				let insertedObjects = notification.userInfo?[NSInsertedObjectsKey] as! Set<NSManagedObject>?
+				$(insertedObjects).$()
+				let deletedObjects = notification.userInfo?[NSDeletedObjectsKey] as! Set<NSManagedObject>?
+				$(deletedObjects).$()
+				try! managedObjectContext.save()
+			}
+		})
+	}
+}
