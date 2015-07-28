d93596c3914ae626aa8ed7549808f075db943049
Grigory Entin <grigory.entin@gmail.com>
2015-07-28 19:11:51 +0300

    Segregated RSSReaderData.

---
+++ RSSReaderData/RSSReaderData.h
@@ -0,0 +1,23 @@
+//
+//  RSSReaderData.h
+//  RSSReaderData
+//
+//  Created by Grigory Entin on 18.07.15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSSReaderData.
+FOUNDATION_EXPORT double RSSReaderDataVersionNumber;
+
+//! Project version string for RSSReaderData.
+FOUNDATION_EXPORT const unsigned char RSSReaderDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSSReaderData/PublicHeader.h>
+
+
+#import "KVOCompliantUserDefaults.h"
+#import "KeyPathRecordingProxy.h"
+#import "KeyPathRecording.h"
+#import "NSBundleExtensions.h"
