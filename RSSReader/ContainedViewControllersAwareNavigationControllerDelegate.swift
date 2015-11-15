ad2211db82774f0dc9078c6832db33bf9a177905
Grigory Entin <grigory.entin@gmail.com>
2015-11-15 01:29:14 +0100

    Added ContainedViewControllersAwareNavigationControllerDelegate.

---
+++ RSSReader/ContainedViewControllersAwareNavigationControllerDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  ContainedViewControllersAwareNavigationControllerDelegate.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 15/11/15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UINavigationController
+
+class ContainedViewControllersAwareNavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
+	func navigationController(navigationController: UINavigationController, didShowViewController viewController: UIViewController, animated: Bool) {
+		$(self).$()
+		viewController.makeContainedViewControllersPerformBlock { containedViewController in
+			containedViewController.viewDidAppearInNavigationController(navigationController, animated: animated)
+		}
+	}
+}
+
+extension UIViewController {
+	func makeContainedViewControllersPerformBlock(block: (viewController: UIViewController) -> ()) {
+		block(viewController: self)
+		for viewController in self.childViewControllers {
+			viewController.makeContainedViewControllersPerformBlock(block)
+		}
+	}
+}
+
+extension UIViewController {
+	func viewDidAppearInNavigationController(navigationController: UINavigationController, animated: Bool) {
+	}
+}
