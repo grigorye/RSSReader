38458490629639ba9f436c2227164b8d9a770a06
Grigory Entin <grigory.entin@gmail.com>
2015-11-16 21:55:49 +0100

    Made bundleOnStack optional.

--- GEBase/GEBase/NSBundleExtensions.swift
+++ GEBase/GEBase/NSBundleExtensions.swift
@@ -9,14 +9,15 @@
 import Foundation
 
 extension NSBundle {
-	static func bundleOnStack() -> NSBundle {
+	static func bundleOnStack() -> NSBundle? {
 		let length = 10
 		let addr = UnsafeMutablePointer<UnsafeMutablePointer<Void>>.alloc(length)
 		let frames = backtrace(addr, Int32(length))
 		assert(2 < frames)
 		var info = Dl_info()
-		let addrFound = dladdr(addr[2], &info)
-		assert(0 != addrFound)
+		guard 0 != dladdr(addr[2], &info) else {
+			return nil
+		}
 		let sharedObjectName = String.fromCString(info.dli_fname)! as NSString
 		let bundle = NSBundle(path: sharedObjectName.stringByDeletingLastPathComponent)!
 		return bundle
