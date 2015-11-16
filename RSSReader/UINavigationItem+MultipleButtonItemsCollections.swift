eccbc29a43aaaceccf1bafb02f35070152eaaf97
Grigory Entin <grigory.entin@gmail.com>
2015-11-16 03:17:33 +0100

    Switched UINavigationItem+MultipleBarButtonItemsCollections to Swift.

---
+++ RSSReader/UINavigationItem+MultipleButtonItemsCollections.swift
@@ -0,0 +1,28 @@
+//
+//  UINavigationItem+MultipleButtonItemsCollections.swift
+//  RSSReader
+//
+//  Created by Grigory Entin on 16/11/15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationItem {
+	@IBOutlet var rightBarButtonItemsCollection: [UIBarButtonItem]? {
+		get {
+			return self.rightBarButtonItems
+		}
+		set {
+			self.rightBarButtonItems = newValue?.sort { $0.tag < $1.tag }
+		}
+	}
+	@IBOutlet var leftBarButtonItemsCollection: [UIBarButtonItem]? {
+		get {
+			return self.leftBarButtonItems
+		}
+		set {
+			self.leftBarButtonItems = newValue?.sort { $0.tag < $1.tag }
+		}
+	}
+}
