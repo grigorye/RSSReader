88cdead9888a72a019252e256dea3b6c31a5b005
Grigory Entin <grigory.entin@gmail.com>
2015-11-19 22:28:09 +0100

    Reincarnated recordedInstanceKeyPath based on unsafeBitCast; embedded KeyPathRecordingProxy manipulation.

--- GEBase/GEBase/KeyPaths.swift
+++ GEBase/GEBase/KeyPaths.swift
@@ -8,17 +8,15 @@
 
 import Foundation
 
-#if false
-public func instanceKeyPath<T: NSObject>(x: T?, recorder: ((T!) -> Void)!) -> String {
-	let keyPath = recordKeyPath { x in
-		recorder(x as! T!)
-	}
+public func recordedInstanceKeyPath<T: NSObject>(x: T?, recorder: ((T!) -> ())!) -> String {
+	let proxy = KeyPathRecordingProxy.newProxy()
+	recorder(unsafeBitCast(proxy, T.self))
+	let keyPath = proxy.keyPathComponents.joinWithSeparator(".")
 	return $(keyPath).$(0)
 }
-#else
+
 public func instanceKeyPath<T: NSObject>(x: T?, recorder: (() -> String)!) -> String {
 	let swiftQuery = recorder()
 	let keyPath = String(swiftQuery.characters.filter { $0 != "!" && $0 != "?" })
 	return $(keyPath).$(0)
 }
-#endif
