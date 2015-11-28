3d79aaaa8be67b5d23ec064d411cdb03ff78f217
Grigory Entin <grigory.entin@gmail.com>
2015-11-28 18:19:56 +0100

    Made .loadDate optional; added validation.

--- RSSReaderData/RSSReaderData/ContainerViewState.swift
+++ RSSReaderData/RSSReaderData/ContainerViewState.swift
@@ -19,23 +19,42 @@ func inversedSortDescriptors(sortDescriptors: [NSSortDescriptor]) -> [NSSortDesc
 }
 
 public class ContainerViewState: NSManagedObject {
+	enum ValidationError: ErrorType {
+		case NeitherLoadDateNorErrorIsSet
+	}
 	@NSManaged public var containerViewPredicate: NSPredicate
     @NSManaged public var continuation: String?
     public var loadError: ErrorType?
-    @NSManaged public var loadDate: NSDate
+    @NSManaged public var loadDate: NSDate?
     @NSManaged public var loadCompleted: Bool
     @NSManaged public var container: Container?
 
 	public var lastLoadedItem: Item? {
+		guard let loadDate = self.loadDate else {
+			return nil
+		}
 		let fetchRequest: NSFetchRequest = {
 			let $ = NSFetchRequest(entityName: "Item")
-			$.predicate = NSPredicate(format: "\(Item.self••{$0.loadDate}) == %@", argumentArray: [self.loadDate])
+			$.predicate = NSPredicate(format: "\(Item.self••{$0.loadDate}) == %@", argumentArray: [loadDate])
 			$.fetchLimit = 1
 			$.sortDescriptors = inversedSortDescriptorsForContainers
 			return $
 		}()
 		let item = try! self.managedObjectContext!.executeFetchRequest(fetchRequest).first as! Item?
 		return item
+	}
+	func validateForUpdateOrInsert() throws {
+		if nil == self.loadDate && nil == self.loadError {
+			throw ValidationError.NeitherLoadDateNorErrorIsSet
+		}
+	}
+	public override func validateForInsert() throws {
+		try super.validateForInsert()
+		try self.validateForUpdateOrInsert()
+	}
+	public override func validateForUpdate() throws {
+		try super.validateForUpdate()
+		try self.validateForUpdateOrInsert()
 	}
 	deinit {
 	}
