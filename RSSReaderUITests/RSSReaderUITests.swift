5ec5121bd4bbfed4ca3687908cca821ab14f955f
Grigory Entin <grigory.entin@gmail.com>
2016-04-17 05:46:23 +0300

    Added tests for opening Subscriptions/History/Favorites and opening first item of the first stream.

--- RSSReaderUITests/RSSReaderUITests.swift
+++ RSSReaderUITests/RSSReaderUITests.swift
@@ -10,28 +10,93 @@ import Foundation
 import XCTest
 
 class RSSReaderUITests: XCTestCase {
-        
+
+	var app = XCUIApplication()
+	lazy var tablesQuery: XCUIElementQuery = self.app.tables
+	lazy var backButton: XCUIElement = self.app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(0)
+	
+	var isAtHome: Bool {
+		return tablesQuery.staticTexts["Subscriptions-AI"].exists
+	}
+	
+    func pushAndPopHomeItemWithAI(itemAI: String) {
+		let homeItem = tablesQuery.staticTexts[itemAI]
+		homeItem.tap()
+		backButton.tap()
+		XCTAssert(isAtHome)
+	}
+	
+	func pushToFirstStreamInSubscriptions() {
+		let subscriptionsItem = tablesQuery.staticTexts["Subscriptions-AI"]
+		subscriptionsItem.tap()
+		while 0 < tablesQuery.count {
+			tablesQuery.cells.elementBoundByIndex(0).tap()
+		}
+		XCTAssert(!isAtHome)
+	}
+	
+	func popToRoot() {
+		while !isAtHome {
+			backButton.tap()
+		}
+	}
+	
+	// MARK:-
+	
+	func repeatForTesting(@noescape block: () -> ()) {
+		for _ in 0..<5 {
+			block()
+		}
+	}
+	
+	// MARK:-
+
+	func testOpenHistory() {
+		pushAndPopHomeItemWithAI("History-AI")
+	}
+	
+	func testOpenSubscriptions() {
+		pushAndPopHomeItemWithAI("Subscriptions-AI")
+	}
+
+	func notestOpenFavorites() {
+		pushAndPopHomeItemWithAI("Favorites-AI")
+	}
+	
+	func testOpenItemInStreamInSubscriptions() {
+		pushToFirstStreamInSubscriptions()
+		popToRoot()
+	}
+
+	// MARK:-
+	
+	func testRepeatOpenHistory() {
+		repeatForTesting { pushAndPopHomeItemWithAI("History-AI") }
+	}
+	
+	func testRepeatOpenSubscriptions() {
+		repeatForTesting { pushAndPopHomeItemWithAI("Subscriptions-AI") }
+	}
+
+	func notestRepeatOpenFavorites() {
+		repeatForTesting { pushAndPopHomeItemWithAI("Favorites-AI") }
+	}
+	
+	func testRepeatOpenItemInStreamInSubscriptions() {
+		repeatForTesting { testOpenItemInStreamInSubscriptions() }
+	}
+	
+	// MARK:-
+	
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+		print(app.debugDescription)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-		XCUIApplication().tables.staticTexts["Subscriptions"].tap()
-#if false
-		XCUIApplication().navigationBars["News"].buttons["Refresh"].tap()
-#endif
-    }
-    
+
 }
