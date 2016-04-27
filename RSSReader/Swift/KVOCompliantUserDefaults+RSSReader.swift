528f3a1879f7bb1072e8805302b9b821df60e468
Grigory Entin <grigory.entin@gmail.com>
2016-04-27 03:34:54 +0300

    Used new name for fetchResultsAnimationEnabled; added groupingTableUpdatesEnabled, updateCellsInPlaceEnabled.

--- RSSReader/Swift/KVOCompliantUserDefaults+RSSReader.swift
+++ RSSReader/Swift/KVOCompliantUserDefaults+RSSReader.swift
@@ -11,19 +11,21 @@ import GEBase
 
 extension KVOCompliantUserDefaults {
 
-	@NSManaged var traceEnabled: Bool
-	@NSManaged var traceLabelsEnabled: Bool
-	@NSManaged var showUnreadOnly: Bool
-	@NSManaged var authToken: String!
-	@NSManaged var login: String!
-	@NSManaged var password: String!
-	@NSManaged var analyticsEnabled: Bool
-	@NSManaged var stateRestorationDisabled: Bool
-	@NSManaged var fetchResultsAreAnimated: Bool
-	@NSManaged var memoryProfilingEnabled: Bool
-	@NSManaged var itemsAreSortedByLoadDate: Bool
 	@NSManaged var foldersLastUpdateDate: NSDate!
 	@NSManaged var foldersLastUpdateErrorEncoded: NSData!
+	//
+	@NSManaged var login: String!
+	@NSManaged var password: String!
+	@NSManaged var showUnreadOnly: Bool
+	//
+	@NSManaged var traceEnabled: Bool
+	@NSManaged var traceLabelsEnabled: Bool
+	@NSManaged var analyticsEnabled: Bool
+	@NSManaged var stateRestorationDisabled: Bool
+	@NSManaged var fetchResultsAnimationEnabled: Bool
+	@NSManaged var groupingTableUpdatesEnabled: Bool
+	@NSManaged var updateCellsInPlaceEnabled: Bool
+	@NSManaged var memoryProfilingEnabled: Bool
 	@NSManaged var pageViewsEnabled: Bool
 	@NSManaged var hideBarsOnSwipe: Bool
 
