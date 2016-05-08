3911338b2b19af0b118dfbfee48a050e83d795cf
Grigory Entin <grigory.entin@gmail.com>
2016-05-09 00:50:00 +0200

    Switched to UTF-16 representation of title/summary.

--- RSSReaderData/RSSReaderData/Item.swift
+++ RSSReaderData/RSSReaderData/Item.swift
@@ -17,8 +17,28 @@ public class Item: NSManagedObject {
 	@NSManaged public var updatedDate: NSDate?
 	@NSManaged public var loadDate: NSDate
 	@NSManaged public var lastOpenedDate: NSDate?
-    @NSManaged public var title: String?
-    @NSManaged public var summary: String?
+    @NSManaged public var titleData: NSData
+    public var title: String {
+		set {
+			let bytes = Array(newValue.utf16)
+			titleData = NSData(bytes: bytes, length: bytes.count * strideof(unichar))
+		}
+		get {
+			let data = titleData
+			return String(utf16CodeUnitsNoCopy: unsafeBitCast(data.bytes, UnsafePointer<unichar>.self), count: data.length / strideof(unichar), freeWhenDone: false)
+		}
+	}
+    @NSManaged public var summaryData: NSData
+    public var summary: String? {
+		set {
+			let bytes = Array(newValue!.utf16)
+			summaryData = NSData(bytes: bytes, length: bytes.count * strideof(unichar))
+		}
+		get {
+			let data = summaryData
+			return String(utf16CodeUnitsNoCopy: unsafeBitCast(data.bytes, UnsafePointer<unichar>.self), count: data.length / strideof(unichar), freeWhenDone: false)
+		}
+	}
 	@NSManaged public var categories: Set<Folder>
 	@NSManaged public var subscription: Subscription
 	@NSManaged public var canonical: [[String: String]]?
