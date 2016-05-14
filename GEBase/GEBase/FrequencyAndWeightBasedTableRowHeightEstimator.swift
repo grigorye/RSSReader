50f2e73c8c0ba507a7af3651e1db8cc1787072c1
Grigory Entin <grigory.entin@gmail.com>
2016-05-14 11:18:31 +0200

    Segregated FrequencyAndWeightBasedTableRowHeightEstimator and TableViewHeightBasedReusedCellGenerator.

---
+++ GEBase/GEBase/FrequencyAndWeightBasedTableRowHeightEstimator.swift
@@ -0,0 +1,42 @@
+//
+//  FrequencyAndWeightBasedTableRowHeightEstimator.swift
+//  GEBase
+//
+//  Created by Grigory Entin on 14/05/16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol FrequencyAndWeightBasedTableRowHeightEstimatorDataSource {
+	associatedtype Weight: Hashable
+	func weightForHeightDefiningValueAtIndexPath(indexPath: NSIndexPath) -> Weight
+}
+
+public struct FrequencyAndWeightBasedTableRowHeightEstimator<DataSource: FrequencyAndWeightBasedTableRowHeightEstimatorDataSource> {
+	public let dataSource: DataSource
+	var frequencyForHeightsByHeightDefiningValueWeight: [DataSource.Weight : [CGFloat : Int]] = [:]
+	// MARK: -
+	public func estimatedRowHeightForItemAtIndexPath(indexPath: NSIndexPath) -> CGFloat? {
+		guard 0 < frequencyForHeightsByHeightDefiningValueWeight.count else {
+			return nil
+		}
+		let weight = dataSource.weightForHeightDefiningValueAtIndexPath(indexPath)
+		let frequencyForHeights = frequencyForHeightsByHeightDefiningValueWeight[weight] ?? [:]
+		let heightAndMaximumFrequency = frequencyForHeights.reduce((0, 0)) {$0.1 > $1.1 ? $0 : $1}
+		guard 0 < heightAndMaximumFrequency.1 else {
+			return nil
+		}
+		return heightAndMaximumFrequency.0
+	}
+	public mutating func addRowHeight(height: CGFloat, forIndexPath indexPath: NSIndexPath) {
+		let weight = dataSource.weightForHeightDefiningValueAtIndexPath(indexPath)
+		var frequencyForHeights = frequencyForHeightsByHeightDefiningValueWeight[weight] ?? [:]
+		frequencyForHeights[height] = (frequencyForHeights[height] ?? 0) + 1
+		frequencyForHeightsByHeightDefiningValueWeight[weight] = frequencyForHeights
+	}
+	public init(dataSource: DataSource) {
+		self.dataSource = dataSource
+	}
+}
---
+++ GEBase/GEBase/TableViewHeightBasedReusedCellGenerator.swift
@@ -0,0 +1,66 @@
+
+//
+//  TableViewDynamicHeightCellEstimator.swift
+//  GEBase
+//
+//  Created by Grigory Entin on 08/05/16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+public extension KVOCompliantUserDefaults {
+	@NSManaged public var cellHeightCachingEnabled: Bool
+}
+
+public protocol TableViewHeightBasedReusedCellGeneratorDataSource {
+	func variableHeightForCell(cell: UITableViewCell) -> CGFloat
+	func isReadyForMeasuringHeigthsForData() -> Bool
+	func variableHeightForDataAtIndexPath(indexPath: NSIndexPath) -> CGFloat
+}
+
+public struct TableViewHeightBasedReusedCellGenerator<DataSource: TableViewHeightBasedReusedCellGeneratorDataSource> {
+	public let dataSource: DataSource
+	public let heightAgnosticCellReuseIdentifier: String
+	public let reuseIdentifiersForHeightCachingCells: [String]
+	// MARK: -
+	var reusedHeights: [CGFloat] = []
+	var reusedHeightsSet: Set<CGFloat> = []
+	var variableHeightsForHeight: [CGFloat : CGFloat] = [:]
+	public func reuseIdentifierForCellForRowAtIndexPath(indexPath: NSIndexPath) -> String {
+		guard dataSource.isReadyForMeasuringHeigthsForData() else {
+			return heightAgnosticCellReuseIdentifier
+		}
+		let variableHeight = dataSource.variableHeightForDataAtIndexPath(indexPath)
+		let heightX: CGFloat? = {
+			for (heightI, variableHeightI) in variableHeightsForHeight {
+				if variableHeightI == variableHeight {
+					return heightI
+				}
+			}
+			return nil
+		}()
+		guard let height = heightX else {
+			return heightAgnosticCellReuseIdentifier
+		}
+		guard let indexInTopReused = reusedHeights.prefix(reuseIdentifiersForHeightCachingCells.count).indexOf(height) else {
+			return heightAgnosticCellReuseIdentifier
+		}
+		return reuseIdentifiersForHeightCachingCells[indexInTopReused]
+	}
+	public mutating func addRowHeight(height: CGFloat, forCell cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+		if !reusedHeightsSet.contains(height) {
+			reusedHeightsSet.insert(height)
+			reusedHeights += [height]
+			variableHeightsForHeight[height] = dataSource.variableHeightForCell(cell)
+			(cell as! SystemLayoutSizeCachingTableViewCell).reused = true
+		}
+	}
+	public init(dataSource: DataSource, heightAgnosticCellReuseIdentifier: String, reuseIdentifiersForHeightCachingCells: [String]) {
+		self.dataSource = dataSource
+		self.heightAgnosticCellReuseIdentifier = heightAgnosticCellReuseIdentifier
+		self.reuseIdentifiersForHeightCachingCells = reuseIdentifiersForHeightCachingCells
+	}
+}
