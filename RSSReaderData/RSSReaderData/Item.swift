1791f66291a90852dd863b39e3da11fde77b1c79
Grigory Entin <grigory.entin@gmail.com>
2016-05-16 22:37:26 +0200

    Added .author.

--- RSSReaderData/RSSReaderData/Item.swift
+++ RSSReaderData/RSSReaderData/Item.swift
@@ -14,6 +14,7 @@ import CoreData
 public class Item: NSManagedObject {
     @NSManaged public var itemID: String
 	@NSManaged public var date: NSDate
+	@NSManaged public var author: String
 	@NSManaged public var updatedDate: NSDate?
 	@NSManaged public var loadDate: NSDate
 	@NSManaged public var lastOpenedDate: NSDate?
