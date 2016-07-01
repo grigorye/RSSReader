c29027a9c037af5de4f481ca2be597a11fcb8a01
Grigory Entin <grigory.entin@gmail.com>
2016-07-01 03:30:06 +0200

    Added logging of application(_, didFinishLaunchingWithOptions:).

--- TestHostApp/TestHostApp/AppDelegate.swift
+++ TestHostApp/TestHostApp/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		NSLog("\(#function)")
 		// Override point for customization after application launch.
 		let splitViewController = self.window!.rootViewController as! UISplitViewController
 		let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
