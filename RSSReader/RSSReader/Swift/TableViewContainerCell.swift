1a17fccb70f480347bf41d83b4721f71d04f69b0
Grigory Entin <grigory.entin@gmail.com>
2016-07-11 00:34:45 +0200

    Added ownItems count into the label.

--- RSSReader/RSSReader/Swift/TableViewContainerCell.swift
+++ RSSReader/RSSReader/Swift/TableViewContainerCell.swift
@@ -14,7 +14,26 @@ class TableViewContainerCell : UITableViewCell {
 	var unreadCountKVOBinding: KVOBinding!
 	func setFromContainer(_ container: Container) {
 		self.unreadCountKVOBinding = KVOBinding(container•#keyPath(Container.unreadCount), options: .initial) {[unowned self] change in
-			self.detailTextLabel?.text = (0 < container.unreadCount) ? "\(container.unreadCount)" : ""
+			let unreadCountText: String? = (0 < container.unreadCount) ? "\(container.unreadCount)" : nil
+			let ownItemsCountText: String? = {
+				guard let itemsOwner = container as? ItemsOwner else {
+					return nil
+				}
+				return "[\(itemsOwner.ownItems.count)]"
+			}()
+			let labelText: String = {
+				if let unreadCountText = unreadCountText, let ownItemsCountText = ownItemsCountText {
+					return ownItemsCountText + " " + unreadCountText
+				}
+				if let ownItemsCountText = ownItemsCountText {
+					return ownItemsCountText
+				}
+				if let unreadCountText = unreadCountText {
+					return unreadCountText
+				}
+				return ""
+			}()
+			self.detailTextLabel?.text = labelText
 		}
 	}
 	override func prepareForReuse() {
