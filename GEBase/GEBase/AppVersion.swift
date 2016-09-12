1176151d25054bca5d7a780e1aab8f7cff78907b
Grigory Entin <grigory.entin@gmail.com>
2016-09-13 00:20:29 +0300

    Added versionIsClean.

---
+++ GEBase/GEBase/AppVersion.swift
@@ -0,0 +1,18 @@
+//
+//  AppVersion.swift
+//  GEBase
+//
+//  Created by Grigory Entin on 11.09.16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+public let versionIsClean: Bool = {
+	let version = Bundle.main.infoDictionary!["CFBundleVersion"] as! NSString
+	$(version)
+	let buildDate = try! FileManager.default.attributesOfItem(atPath: Bundle.main.bundlePath)[FileAttributeKey.modificationDate] as! Date
+	let buildAge = Date().timeIntervalSince(buildDate)
+	$(buildAge)
+	return NSNotFound == version.rangeOfCharacter(from: NSCharacterSet.decimalDigits.inverted).location
+}()
