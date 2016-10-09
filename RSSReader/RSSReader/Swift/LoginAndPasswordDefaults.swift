8b0db85aee33a0f2887d49b69c6d535c1f91775e
Grigory Entin <grigory.entin@gmail.com>
2016-10-09 15:38:16 +0200

    Split KVOCompliantUserDefaults extensions to the sources.

--- RSSReader/RSSReader/Swift/LoginAndPasswordDefaults.swift
+++ RSSReader/RSSReader/Swift/LoginAndPasswordDefaults.swift
@@ -1,5 +1,5 @@
 //
-//  UserDefaults.swift
+//  LoginAndPasswordDefaults.swift
 //  RSSReader
 //
 //  Created by Grigory Entin on 03.01.15.
@@ -11,6 +11,9 @@ import GEBase
 import Foundation
 
 extension KVOCompliantUserDefaults {
+	@NSManaged var login: String!
+	@NSManaged var password: String!
+	//
 	var loginAndPassword: LoginAndPassword {
 		return LoginAndPassword(login: self.login, password: self.password)
 	}
