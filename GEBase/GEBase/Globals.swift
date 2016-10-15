d97eb001b725f7094793df99b2297db6d388d863
Grigory Entin <grigory.entin@gmail.com>
2016-10-15 21:27:17 +0200

    Moved in urlTaskGeneratorProgressBinding.

--- GEBase/GEBase/Globals.swift
+++ GEBase/GEBase/Globals.swift
@@ -6,8 +6,17 @@
 //  Copyright © 2015 Grigory Entin. All rights reserved.
 //
 
+import UIKit
 import Foundation
 
 public let defaults = KVOCompliantUserDefaults()
 
 public let progressEnabledURLSessionTaskGenerator = ProgressEnabledURLSessionTaskGenerator()
+
+public let urlTaskGeneratorProgressBinding: AnyObject = {
+	let taskGenerator = progressEnabledURLSessionTaskGenerator
+	return KVOBinding(taskGenerator•#keyPath(ProgressEnabledURLSessionTaskGenerator.progresses), options: []) { change in
+		let networkActivityIndicatorShouldBeVisible = 0 < taskGenerator.progresses.count
+		UIApplication.shared.isNetworkActivityIndicatorVisible = (networkActivityIndicatorShouldBeVisible)
+	}
+}()
