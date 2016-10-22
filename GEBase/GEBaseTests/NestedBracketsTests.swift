62d780f73686313d01b7d80b72f04bba72975612
Grigory Entin <grigory.entin@gmail.com>
2016-10-22 22:32:13 +0200

    Added more tests.

--- GEBase/GEBaseTests/NestedBracketsTests.swift
+++ GEBase/GEBaseTests/NestedBracketsTests.swift
@@ -8,13 +8,34 @@
 
 import XCTest
 
-class StringTests : XCTestCase {
-	let s = "a.b(c))"
+class NestedBracketsTests : XCTestCase {
+	let a = "a.b(c))"
+	let b = "a.b(c)"
+	let c = "a.b(c"
+	let d = "a.b)c("
 	let i = "0123456"
-	func testClosingBracketRange() {
+	func testClosingBracketRangeWithSecondBracket() {
 		XCTAssertEqual(
-			s.rangeOfClosingBracket(")", openingBracket: "(")!.lowerBound,
-			s.index(s.startIndex, offsetBy: 6)
+			a.rangeOfClosingBracket(")", openingBracket: "(")!.lowerBound,
+			a.index(a.startIndex, offsetBy: 6)
+		)
+	}
+	func testClosingBracketRangeWithNoBracket() {
+		XCTAssertEqual(
+			b.rangeOfClosingBracket(")", openingBracket: "("),
+			nil
+		)
+	}
+	func testClosingBracketRangeWithNoBracketAndOpeningBracket() {
+		XCTAssertEqual(
+			c.rangeOfClosingBracket(")", openingBracket: "("),
+			nil
+		)
+	}
+	func testClosingBracketRangeWithFirstBracketAndOpeningBracket() {
+		XCTAssertEqual(
+			d.rangeOfClosingBracket(")", openingBracket: "(")!.lowerBound,
+			d.index(a.startIndex, offsetBy: 3)
 		)
 	}
 }
