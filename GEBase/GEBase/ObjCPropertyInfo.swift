716e6df3882e3c4d6675c421d117ab8386b9e3e6
Grigory Entin <grigory.entin@gmail.com>
2016-10-23 01:07:15 +0200

    Renamed ObjectiveCBridging to ObjCPropertyInfo; moved out ObjCEncode.

--- GEBase/GEBase/ObjCPropertyInfo.swift
+++ GEBase/GEBase/ObjCPropertyInfo.swift
@@ -1,5 +1,5 @@
 //
-//  ObjectiveCBridging.swift
+//  ObjCPropertyInfo.swift
 //  GEBase
 //
 //  Created by Grigory Entin on 02.04.16.
@@ -8,28 +8,8 @@
 
 import Foundation
 
-public func objCEncode<T>(_ type: T.Type) -> String {
-	switch type {
-	case is Int.Type:
-		return String(validatingUTF8: (1 as NSNumber).objCType)!
-	case is Bool.Type:
-		return String(validatingUTF8: (true as NSNumber).objCType)!
-	case is AnyObject.Type:
-		return "@"
-	default:
-		abort()
-	}
-}
-
 public func objCDefaultSetterName(forPropertyName propertyName: String) -> String {
 	return "set\(propertyName.uppercased().characters.first!)\(propertyName.substring(from: propertyName.index(after: propertyName.startIndex))):"
-}
-
-public func objCValue(forProperty property: objc_property_t, attributeName: String) -> String? {
-	let valueCString = property_copyAttributeValue(property, attributeName)!
-	let $ = String(validatingUTF8: valueCString)
-	free(valueCString)
-	return $;
 }
 
 struct PropertyInfo {
