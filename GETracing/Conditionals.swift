2143d256501e1644ab412128b0abba4cb018e259
Grigory Entin <grigory.entin@gmail.com>
2016-10-24 21:26:30 +0200

    Segregated Conditionals.swift.

---
+++ GETracing/Conditionals.swift
@@ -0,0 +1,79 @@
+//
+//  Conditionals.swift
+//  GETracing
+//
+//  Created by Grigory Entin on 24.10.16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+public var filesWithTracingDisabled = [String]()
+
+public func disableTrace(file: String = #file, function: String = #function) -> Any? {
+	guard traceEnabled else {
+		return nil
+	}
+	return TraceLocker(file: file, function: function)
+}
+
+public func enableTrace(file: String = #file, function: String = #function) -> Any? {
+	guard traceEnabled else {
+		return nil
+	}
+	return TraceUnlocker(file: file, function: function)
+}
+
+func tracingShouldBeEnabledForLocation(_ location: SourceLocation) -> Bool {
+	guard !filesWithTracingDisabled.contains(location.fileURL.lastPathComponent) else {
+		return false
+	}
+	guard 0 == (traceLockCountForFileAndFunction[location.fileAndFunction] ?? 0) else {
+		return false
+	}
+	return true
+}
+
+private var traceLockCountForFileAndFunction: [SourceFileAndFunction : Int] = [:]
+
+private class TraceLocker {
+
+	let sourceFileAndFunction: SourceFileAndFunction
+	
+	init(file: String = #file, function: String = #function) {
+		self.sourceFileAndFunction = SourceFileAndFunction(fileURL: URL(fileURLWithPath: file), function: function)
+		let oldValue = traceLockCountForFileAndFunction[self.sourceFileAndFunction] ?? 0
+		traceLockCountForFileAndFunction[self.sourceFileAndFunction] = oldValue + 1
+	}
+	
+	deinit {
+		let oldValue = traceLockCountForFileAndFunction[self.sourceFileAndFunction]!
+		traceLockCountForFileAndFunction[self.sourceFileAndFunction] = oldValue - 1
+	}
+	
+}
+
+private class TraceUnlocker {
+
+	let sourceFileAndFunction: SourceFileAndFunction
+	
+	let unlockingWithNoLock: Bool
+	
+	init(file: String = #file, function: String = #function) {
+		self.sourceFileAndFunction = SourceFileAndFunction(fileURL: URL(fileURLWithPath: file), function: function)
+		let oldValue = traceLockCountForFileAndFunction[self.sourceFileAndFunction] ?? 0
+		let unlockingWithNoLock = oldValue == 0
+		if !unlockingWithNoLock {
+			traceLockCountForFileAndFunction[self.sourceFileAndFunction] = oldValue - 1
+		}
+		self.unlockingWithNoLock = unlockingWithNoLock
+	}
+	
+	deinit {
+		if !self.unlockingWithNoLock {
+			let oldValue = traceLockCountForFileAndFunction[self.sourceFileAndFunction]!
+			traceLockCountForFileAndFunction[self.sourceFileAndFunction] = oldValue + 1
+		}
+	}
+	
+}
