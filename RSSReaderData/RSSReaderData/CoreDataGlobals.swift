44ebe38bdaed29612c2d0083ef5ea0c99529c1b5
Grigory Entin <grigory.entin@gmail.com>
2016-11-19 14:06:41 +0100

    Added defaults.savingDisabled; segregated main/background/save queue managed object context initialization; segregated regeneratedPSC.

--- RSSReaderData/RSSReaderData/CoreDataGlobals.swift
+++ RSSReaderData/RSSReaderData/CoreDataGlobals.swift
@@ -16,77 +16,84 @@ extension KVOCompliantUserDefaults {
 	@NSManaged var coreDataCachingEnabled: Bool
 	@NSManaged var backgroundImportEnabled: Bool
 	@NSManaged var forceStoreRemoval: Bool
+	@NSManaged var savingDisabled: Bool
 }
 
-public let (managedObjectContextError, saveQueueManagedObjectContext, optionalMainQueueManagedObjectContext, optionalBackgroundQueueManagedObjectContext, supplementaryObjects): (Error?, NSManagedObjectContext?, NSManagedObjectContext?, NSManagedObjectContext?, [Any]) = {
-	do {
-		let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: NSClassFromString("RSSReaderData.Folder")!)])!
-		let psc = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+private func regeneratedPSC() throws -> NSPersistentStoreCoordinator {
+	let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: NSClassFromString("RSSReaderData.Folder")!)])!
+	let psc = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+	let fileManager = FileManager.default
+	let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+	let documentsDirectoryURL = urls[0]
+	if !fileManager.fileExists(atPath: documentsDirectoryURL.path) {
+		try fileManager.createDirectory(at: documentsDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+	}
+	let storeURL = documentsDirectoryURL.appendingPathComponent("RSSReaderData.sqlite")
+	$(fileManager.fileExists(atPath: storeURL.path))
+	if defaults.forceStoreRemoval {
 		let fileManager = FileManager.default
-		let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
-		let documentsDirectoryURL = urls[0]
-		if !fileManager.fileExists(atPath: documentsDirectoryURL.path) {
-			try fileManager.createDirectory(at: documentsDirectoryURL, withIntermediateDirectories: true, attributes: nil)
-		}
-		let storeURL = documentsDirectoryURL.appendingPathComponent("RSSReaderData.sqlite")
-		$(fileManager.fileExists(atPath: storeURL.path))
-		if defaults.forceStoreRemoval {
-			let fileManager = FileManager.default
-			do {
-				try fileManager.removeItem(at: storeURL)
-			}
-			catch CocoaError.fileNoSuchFile {
-			}
-		}
 		do {
-			let options = [
-				NSMigratePersistentStoresAutomaticallyOption: true,
-				NSInferMappingModelAutomaticallyOption: true
-			]
-			let persistentStore = try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
-		} catch let error as CocoaError {
-			switch error.code.rawValue {
-			case NSMigrationMissingSourceModelError where UserDefaults().bool(forKey: "allowMissingSourceModelError"):
-			fallthrough
-			case NSPersistentStoreIncompatibleVersionHashError, NSMigrationError:
-				let fileManager = FileManager.default
-				try fileManager.removeItem(at: storeURL)
-				try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
-			default:
-				throw error
-			}
+			try fileManager.removeItem(at: storeURL)
 		}
-		let saveQueueManagedObjectContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
-			$0.name = "save"
-			$0.persistentStoreCoordinator = psc
+		catch CocoaError.fileNoSuchFile {
 		}
-		let mainQueueManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType) … {
-			$0.name = "main"
-			$0.parent = saveQueueManagedObjectContext
-		}
-		let backgroundQueueManagedObjectContext: NSManagedObjectContext = {
-			guard defaults.backgroundImportEnabled else {
-				return mainQueueManagedObjectContext
-			}
-			return NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
-				$0.name = "background"
-				$0.parent = mainQueueManagedObjectContext
-				if defaults.coreDataCachingEnabled {
-					$0.cachingEnabled = true
-				}
-			}
-		}()
-		var supplementaryObjects = [Any]()
-		supplementaryObjects += [
-			ManagedObjectContextAutosaver(managedObjectContext: mainQueueManagedObjectContext, queue: nil),
-			ManagedObjectContextAutosaver(managedObjectContext: saveQueueManagedObjectContext, queue: nil)
+	}
+	do {
+		let options = [
+			NSMigratePersistentStoresAutomaticallyOption: true,
+			NSInferMappingModelAutomaticallyOption: true
 		]
-		return (nil, saveQueueManagedObjectContext, mainQueueManagedObjectContext, backgroundQueueManagedObjectContext, supplementaryObjects)
+		_ = try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
+	} catch let error as CocoaError {
+		switch error.code.rawValue {
+		case NSMigrationMissingSourceModelError where UserDefaults().bool(forKey: "allowMissingSourceModelError"):
+		fallthrough
+		case NSPersistentStoreIncompatibleVersionHashError, NSMigrationError:
+			let fileManager = FileManager.default
+			try fileManager.removeItem(at: storeURL)
+			try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+		default:
+			throw error
+		}
+	}
+	return psc
+}
+
+private var saveQueueMOCAutosaver: ManagedObjectContextAutosaver?
+public let (managedObjectContextError, saveQueueManagedObjectContext): (Error?, NSManagedObjectContext?) = {
+	do {
+		let saveQueueManagedObjectContext = try NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
+			$0.name = "save"
+			$0.persistentStoreCoordinator = try regeneratedPSC()
+		}
+		if !defaults.savingDisabled {
+			saveQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: saveQueueManagedObjectContext, queue: nil)
+		}
+		return (nil, saveQueueManagedObjectContext)
 	}
 	catch {
-		return (error, nil, nil, nil, [])
+		return (error, nil)
 	}
 }()
 
-public let mainQueueManagedObjectContext = { optionalMainQueueManagedObjectContext! }()
-public let backgroundQueueManagedObjectContext = { optionalBackgroundQueueManagedObjectContext! }()
+private var mainQueueMOCAutosaver: ManagedObjectContextAutosaver?
+public let mainQueueManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType) … {
+	$0.name = "main"
+	$0.parent = saveQueueManagedObjectContext
+	if !defaults.savingDisabled {
+		mainQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: $0, queue: nil)
+	}
+}
+
+public let backgroundQueueManagedObjectContext: NSManagedObjectContext = {
+	guard defaults.backgroundImportEnabled else {
+		return mainQueueManagedObjectContext
+	}
+	return NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
+		$0.name = "background"
+		$0.parent = mainQueueManagedObjectContext
+		if defaults.coreDataCachingEnabled {
+			$0.cachingEnabled = true
+		}
+	}
+}()
