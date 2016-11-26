5f7e9b3c01cfbcbffb567fb226bb0af73e40cfd6
Grigory Entin <grigory.entin@gmail.com>
2016-11-26 14:45:12 +0100

    Added SourceExtractedInfo with support for playgrounds.

--- GETracing/GETracing/Logging.swift
+++ GETracing/GETracing/Logging.swift
@@ -9,11 +9,30 @@
 import Foundation
 
 /// Nothing more than a wrapper around log parameters.
+
+public struct SourceExtractedInfo {
+	public let label: String
+	public let playgroundName: String?
+	init(label: String, playgroundName: String? = nil) {
+		self.label = label
+		self.playgroundName = playgroundName
+	}
+}
+
 public struct LogRecord {
 	public let message: String
-	public let label: String?
+	public let sourceExtractedInfo: SourceExtractedInfo
 	public let date: Date
 	public let location: SourceLocation
+}
+
+extension LogRecord {
+	public var label: String! {
+		return self.sourceExtractedInfo.label
+	}
+	public var playgroundName: String? {
+		return self.sourceExtractedInfo.playgroundName
+	}
 }
 
 public typealias Logger = (LogRecord) -> ()
@@ -23,9 +42,9 @@ public var loggers: [Logger] = [
 ]
 
 func log<T>(_ value: T, on date: Date, at location: SourceLocation) {
-	let label = GETracing.label(for: location)
+	let sourceExtractedInfo = GETracing.sourceExtractedInfo(for: location)
 	let message = descriptionImp(of: value)
-	let record = LogRecord(message: message, label: label, date: date, location: location)
+	let record = LogRecord(message: message, sourceExtractedInfo: sourceExtractedInfo, date: date, location: location)
 	for logger in loggers {
 		logger(record)
 	}
