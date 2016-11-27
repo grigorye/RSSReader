df60304322472cbf43899d8eee45de80c6c15d4c
Grigory Entin <grigory.entin@gmail.com>
2016-11-27 14:16:39 +0100

    Moved in $; added z$.

--- GETracing/GETracing/Conveniences.swift
+++ GETracing/GETracing/Conveniences.swift
@@ -44,3 +44,43 @@ import Foundation
 public prefix func •<T>(argument: @autoclosure () -> T) -> Void {
 }
 prefix operator •
+
+/// Passes-through `value`, logging it as necessary with `loggers`.
+///
+/// Consider Baz.swift:
+/// ````
+/// func sinPi() -> Float {
+///     let foo = Float.pi
+///     let bar = sin(foo)
+///     return bar
+/// }
+/// ````
+/// Any expression used in the code might be logged by simply wrapping it in `$()`:
+/// ````
+/// func sinPi() -> Float {
+///     let foo = Float.pi
+///     $(cos(foo))
+///     let bar = sin($(foo))
+///     return bar
+/// }
+/// ````
+/// When `sinPi` is executed, value for `cos(foo)` as well as `foo` passed to `sin` may be logged as below:
+/// ````
+/// 03:12.13.869 [-] sinPi, Baz.swift:4, cos(foo): -1
+/// 03:12.13.855 [-] sinPi, Baz.swift:5, foo: 3.141593
+/// ````
+/// - seealso: `•`.
+/// - seealso: `loggers`.
+@discardableResult
+public func $<T>(file: String = #file, line: Int = #line, column: Int = #column, function: String = #function, dso: UnsafeRawPointer = #dsohandle, _ valueClosure: @autoclosure () -> T) -> T {
+	let value = valueClosure()
+	traceAsNecessary(value, file: file, line: line, column: column, function: function, moduleReference: .dso(dso), traceFunctionName: "$")
+	return value
+}
+
+discardableResult
+public func z$<T>(file: String = #file, line: Int = #line, column: Int = #column, function: String = #function, dso: UnsafeRawPointer = #dsohandle, _ valueClosure: @autoclosure () -> T) -> T {
+	let value = valueClosure()
+	traceAsNecessary(value, file: file, line: line, column: column, function: function, moduleReference: .dso(dso), traceFunctionName: "z$")
+	return value
+}
