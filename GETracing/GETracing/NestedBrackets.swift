a7c945f9036f37b31f5e1b39c0d1fb85e870a877
Grigory Entin <grigory.entin@gmail.com>
2016-11-27 14:15:08 +0100

    Added followedBy to allow nested expressions.

--- GETracing/GETracing/NestedBrackets.swift
+++ GETracing/GETracing/NestedBrackets.swift
@@ -10,15 +10,22 @@ import Foundation
 
 extension String {
 	/// Returns the range of the first occurence of a given closing bracket that is not paired with a given opening bracket.
-	public func rangeOfClosingBracket(_ closingBracket: String, openingBracket: String, range searchRange: Range<Index>? = nil) -> Range<String.CharacterView.Index>? {
+	public func rangeOfClosingBracket(_ closingBracket: String, openingBracket: String, followedBy: String? = nil, range searchRange: Range<Index>? = nil) -> Range<String.CharacterView.Index>? {
 		guard let openingBracketRange = range(of: openingBracket, range: searchRange) else {
 			return range(of: closingBracket, range: searchRange)
 		}
 		guard let closingBracketRange = range(of: closingBracket, range: searchRange) else {
 			return nil
 		}
-		guard openingBracketRange.lowerBound < closingBracketRange.lowerBound else {
-			return closingBracketRange
+		if openingBracketRange.lowerBound > closingBracketRange.lowerBound {
+			if let followedBy = followedBy {
+				if substring(from: closingBracketRange.lowerBound).hasPrefix(followedBy) {
+					return closingBracketRange
+				}
+			}
+			else {
+				return closingBracketRange
+			}
 		}
 		let upperBound = searchRange?.upperBound ?? self.characters.endIndex
 		let tailIndex = openingBracketRange.upperBound
