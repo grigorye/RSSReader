c5fc9e35516ff6acc3c80dfc86064c0fec23b4d3
Grigory Entin <grigory.entin@gmail.com>
2016-11-29 01:10:41 +0100

    Added TestHostApp-MacOS.

---
+++ TestHostApp/TestHostApp-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestHostApp-macOS
+//
+//  Created by Grigory Entin on 29.11.16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
