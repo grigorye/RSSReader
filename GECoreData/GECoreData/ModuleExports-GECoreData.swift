d086734000dbc9152ab96d69db89135e4b24d023
Grigory Entin <grigory.entin@gmail.com>
2016-12-09 02:40:33 +0100

    Added ModuleExports-XXX/GECoreData.swift.

---
+++ GECoreData/GECoreData/ModuleExports-GECoreData.swift
@@ -0,0 +1,24 @@
+//
+//  ModuleExports-GECoreData.swift
+//  GECoreData
+//
+//  Created by Grigory Entin on 09.12.16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import GECoreData
+import CoreData
+
+typealias DefaultSortable = GECoreData.DefaultSortable
+typealias ManagedIdentifiable = GECoreData.ManagedIdentifiable
+typealias ManagedObjectContextAutosaver = GECoreData.ManagedObjectContextAutosaver
+typealias FetchedObjectBinding = GECoreData.FetchedObjectBinding
+typealias FetchedObjectCountBinding = GECoreData.FetchedObjectCountBinding
+
+func typedObjectID<T: NSManagedObject>(for object: T) -> TypedManagedObjectID<T> {
+	return GECoreData.typedObjectID(for: object)
+}
+
+func typedObjectID<T: NSManagedObject>(for object: T?) -> TypedManagedObjectID<T>? {
+	return GECoreData.typedObjectID(for: object)
+}
