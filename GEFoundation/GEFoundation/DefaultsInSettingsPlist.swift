15d2794189bb5ad6085f170988cc13ba9c1977f1
Grigory Entin <grigory.entin@gmail.com>
2016-12-10 12:11:28 +0100

    Switched loadDefaultsFromSettingsPlistAtURL from NSDictionar; fixed interpretation of bad keys in loadDefaultsFromSettingsPlistAtURL.

--- GEFoundation/GEFoundation/DefaultsInSettingsPlist.swift
+++ GEFoundation/GEFoundation/DefaultsInSettingsPlist.swift
@@ -10,16 +10,16 @@ import Foundation
 
 public func loadDefaultsFromSettingsPlistAtURL(_ url: URL) throws {
 	let data = try Data(contentsOf: url, options: NSData.ReadingOptions(rawValue: 0))
-    let settingsPlist = try PropertyListSerialization.propertyList(from: data, options: PropertyListSerialization.ReadOptions(), format: nil) as! NSDictionary
+    let settingsPlist = try PropertyListSerialization.propertyList(from: data, options: PropertyListSerialization.ReadOptions(), format: nil) as! [String : AnyObject]
     let preferencesSpecifiers = settingsPlist["PreferenceSpecifiers"] as! [[String : AnyObject]]
 	let defaultKeysAndValuesForRegistration: [(key: String, defaultValue: AnyObject)] = preferencesSpecifiers.flatMap {
-		guard let key = $0["Key"] as! String? else {
+		guard let key = $0["Key"] as? String else {
 			return nil
 		}
 		let defaultValue = $0["DefaultValue"]
 		return (key: key, defaultValue: defaultValue!)
 	}
-	let defaultsForRegistration = defaultKeysAndValuesForRegistration.reduce([String : AnyObject]()) {
+	let defaultsForRegistration: [String : AnyObject] = defaultKeysAndValuesForRegistration.reduce([String : AnyObject]()) {
 		var $ = $0
 		$[$1.key] = $1.defaultValue
 		return $
