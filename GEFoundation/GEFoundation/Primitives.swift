7b6a57cb12a2777e9f8de29826695446768ad1d3
Grigory Entin <grigory.entin@gmail.com>
2016-12-17 03:01:16 +0100

    Added ScheduledHandlers.

--- GEFoundation/GEFoundation/Primitives.swift
+++ GEFoundation/GEFoundation/Primitives.swift
@@ -11,3 +11,27 @@ public var _0 = false
 
 public typealias Handler = () -> Void
 
+private func performAndDropEachReversed(_ array: inout [Handler]) {
+	for _ in 0..<array.count {
+		let handler = array.last!
+		handler()
+		array = Array(array.dropLast(1))
+	}
+}
+
+public struct ScheduledHandlers {
+
+	var handlers = [Handler]()
+	
+	public mutating func perform() {
+		performAndDropEachReversed(&handlers)
+	}
+	
+	public init() {}
+	
+}
+
+public func += (handlers: ScheduledHandlers, _ extraHandlers: [Handler]) {
+	handlers += extraHandlers
+}
+
