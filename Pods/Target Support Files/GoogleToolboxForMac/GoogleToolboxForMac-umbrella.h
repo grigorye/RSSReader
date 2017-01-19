972740c810c7ffd00b088444bfa9728dce0991c2
Grigory Entin <grigory.entin@gmail.com>
2017-01-19 10:36:36 +0100

    Added Firebase/Core; removed TUSafariActivity.

---
+++ Pods/Target Support Files/GoogleToolboxForMac/GoogleToolboxForMac-umbrella.h
@@ -0,0 +1,42 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GTMABAddressBook.h"
+#import "GTMTypeCasting.h"
+#import "GTMLocalizedString.h"
+#import "GTMLogger.h"
+#import "GTMDebugSelectorValidation.h"
+#import "GTMDebugThreadValidation.h"
+#import "GTMMethodCheck.h"
+#import "GTMDefines.h"
+#import "GTMGeometryUtils.h"
+#import "GTMNSObject+KeyValueObserving.h"
+#import "GTMLogger.h"
+#import "GTMNSData+zlib.h"
+#import "GTMNSDictionary+URLArguments.h"
+#import "GTMNSFileHandle+UniqueName.h"
+#import "GTMNSScanner+JSON.h"
+#import "GTMNSString+HTML.h"
+#import "GTMNSString+URLArguments.h"
+#import "GTMNSString+XML.h"
+#import "GTMNSThread+Blocks.h"
+#import "GTMRegex.h"
+#import "GTMRoundedRectPath.h"
+#import "GTMStackTrace.h"
+#import "GTMStringEncoding.h"
+#import "GTMSystemVersion.h"
+#import "GTMUIFont+LineHeight.h"
+#import "GTMURLBuilder.h"
+#import "GTMFoundationUnitTestingUtilities.h"
+#import "GTMSenTestCase.h"
+#import "GTMTestTimer.h"
+#import "GTMCodeCoverageApp.h"
+#import "GTMIPhoneUnitTestDelegate.h"
+#import "GTMFadeTruncatingLabel.h"
+#import "GTMUIImage+Resize.h"
+#import "GTMUILocalizer.h"
+
+FOUNDATION_EXPORT double GoogleToolboxForMacVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleToolboxForMacVersionString[];
+
