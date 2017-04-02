ebe14666588214df086e822acad18f12b3be81c8
Grigory Entin <grigory.entin@gmail.com>
2017-04-02 14:50:55 +0200

    Segregated loadPersistentStoresInteractively; added load error recovery via loadPersistentStoresAfterRemovalInteractively.

--- RSSReader/RSSReader/Swift/AppDelegate.swift
+++ RSSReader/RSSReader/Swift/AppDelegate.swift
@@ -18,6 +18,43 @@ class AppDelegate : AppDelegateBase {
 		$(self)
 		return true
 	}
+	func loadPersistentStoresInteractively(completion: @escaping (Error?) -> ()) {
+		var scope = Activity("Loading Persistent Stores").enter()
+		loadPersistentStores { error in
+			guard let loadPersistentStoresError = error else {
+				scope.leave()
+				return
+			}
+			trackError($(loadPersistentStoresError))
+			DispatchQueue.main.async {
+				$(loadPersistentStoresError)
+				self.loadPersistentStoresAfterRemovalInteractively(completion: completion)
+				scope.leave()
+			}
+		}
+	}
+	func loadPersistentStoresAfterRemovalInteractively(completion: @escaping (Error?) -> ()) {
+		do {
+			var scope = Activity("Erasing persistent stores as part of recovery").enter(); defer { scope.leave() }
+			try erasePersistentStores()
+		} catch {
+			completion(error)
+			return
+		}
+		var scope = Activity("Loading persistent stores after erasing").enter()
+		loadPersistentStores { error in
+			DispatchQueue.main.async {
+				defer { scope.leave() }
+				guard let loadPersistentStoresError = error else {
+					presentErrorMessage(NSLocalizedString("Something went wrong. Offline data has been erased.", comment: ""))
+					return
+				}
+				$(loadPersistentStoresError)
+				presentErrorMessage(NSLocalizedString("Something went wrong. Offline data might be unavailable. Please re-install the application to avoid further problems.", comment: ""))
+			}
+		}
+	}
+	// MARK: -
 	override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
 		defer { launchingScope.leave() }
 		var scope = Activity("Finishing Launching").enter(); defer { scope.leave() }
@@ -25,17 +62,8 @@ class AppDelegate : AppDelegateBase {
 			return false
 		}
 		do {
-			var scope = Activity("Loading Persistent Stores").enter()
-			loadPersistentStores { error in
-				guard let loadPersistentStoresError = error else {
-					scope.leave()
-					return
-				}
-				DispatchQueue.main.async {
-					$(loadPersistentStoresError)
-					presentErrorMessage(NSLocalizedString("Something went wrong.", comment: ""))
-					scope.leave()
-				}
+			loadPersistentStoresInteractively { error in
+				_ = $(error)
 			}
 		}
 		_ = mainScene
