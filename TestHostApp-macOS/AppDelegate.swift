b8bd60b07c97df96ec712ab88648ced71980fe5d
Grigory Entin <grigory.entin@gmail.com>
2017-04-10 00:22:33 +0200

    Squashed 'TestHostApp/' content from commit 52c06da
    git-subtree-dir: TestHostApp
    git-subtree-split: 52c06da633897ed4b983ce880b15abe93528e152

---
+++ TestHostApp-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestHostApp-macOS
+//
+//  Created by Grigory Entin on 29.11.16.
+//  Copyright © 2016 Grigory Entin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
