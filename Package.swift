3de3599868337b0f6822234e6725a82d5f49e2eb
Matt Gallagher <matt@zqueue.com>
2017-05-14 02:10:33 +1000

    Included the ReferenceRandomGenerators target in the dependencies for the primary target in the Swift package manager.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "CwlUtils",
 	targets: [
-		Target(name: "CwlUtils", dependencies: ["CwlFrameAddress"]),
+		Target(name: "CwlUtils", dependencies: ["CwlFrameAddress", "ReferenceRandomGenerators"]),
 		Target(name: "CwlFrameAddress"),
 		Target(name: "ReferenceRandomGenerators")
 	],
