367287056a44cae7bcd1d852d4a817c6722cc0a0
Matt Gallagher <matt@zqueue.com>
2017-06-13 18:10:48 +1000

    Minor tidy up of dependency fetching output format.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
 	name: "CwlUtils",
 	products: [.library(name: "CwlUtils", targets: ["CwlUtils", "CwlFrameAddress"])],
 	dependencies: [
-		.package(url: "/Users/matt/Projects/CwlPreconditionTesting", .revision("17db9e179a764608f07d215694e0b4d8abea9cf7")),
+		.package(url: "/Users/matt/Projects/CwlPreconditionTesting", .revision("daa6e033b8106bed90fed2558e7586675ff63288")),
 	],
 	targets: [
 		.target(name: "ReferenceRandomGenerators"),
