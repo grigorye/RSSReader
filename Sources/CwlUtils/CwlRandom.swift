6e085da723b30387f5896a1680ef3b79ffe25062
Matt Gallagher <matt@zqueue.com>
2017-06-22 11:33:22 +1000

    Moved unneeded random implementations to testing targets.

--- Sources/CwlUtils/CwlRandom.swift
+++ Sources/CwlUtils/CwlRandom.swift
@@ -191,106 +191,6 @@ public struct Arc4Random: RandomGenerator {
 	}
 }
 
-public struct Lfsr258: RandomWordGenerator {
-	public typealias WordType = UInt64
-	public typealias StateType = (UInt64, UInt64, UInt64, UInt64, UInt64)
-
-	static let k: (UInt64, UInt64, UInt64, UInt64, UInt64) = (1, 9, 12, 17, 23)
-	static let q: (UInt64, UInt64, UInt64, UInt64, UInt64) = (1, 24, 3, 5, 3)
-	static let s: (UInt64, UInt64, UInt64, UInt64, UInt64) = (10, 5, 29, 23, 8)
-
-	var state: StateType = (0, 0, 0, 0, 0)
-
-	public init() {
-		var r = DevRandom()
-		repeat {
-			r.randomize(buffer: &state.0, size: MemoryLayout<UInt64>.size)
-		} while state.0 < Lfsr258.k.0
-		repeat {
-			r.randomize(buffer: &state.1, size: MemoryLayout<UInt64>.size)
-		} while state.1 < Lfsr258.k.1
-		repeat {
-			r.randomize(buffer: &state.2, size: MemoryLayout<UInt64>.size)
-		} while state.2 < Lfsr258.k.2
-		repeat {
-			r.randomize(buffer: &state.3, size: MemoryLayout<UInt64>.size)
-		} while state.3 < Lfsr258.k.3
-		repeat {
-			r.randomize(buffer: &state.4, size: MemoryLayout<UInt64>.size)
-		} while state.4 < Lfsr258.k.4
-	}
-	
-	public init(seed: StateType) {
-		self.state = seed
-	}
-	
-	public mutating func randomWord() -> UInt64 {
-		return random64()
-	}
-
-	public mutating func random64() -> UInt64 {
-		// Constants from "Tables of Maximally-Equidistributed Combined LFSR Generators" by Pierre L'Ecuyer:
-		// http://www.iro.umontreal.ca/~lecuyer/myftp/papers/tausme2.ps
-		let l: UInt64 = 64
-		let x0 = (((state.0 << Lfsr258.q.0) ^ state.0) >> (l - Lfsr258.k.0 - Lfsr258.s.0))
-		state.0 = ((state.0 & (UInt64.max << Lfsr258.k.0)) << Lfsr258.s.0) | x0
-		let x1 = (((state.1 << Lfsr258.q.1) ^ state.1) >> (l - Lfsr258.k.1 - Lfsr258.s.1))
-		state.1 = ((state.1 & (UInt64.max << Lfsr258.k.1)) << Lfsr258.s.1) | x1
-		let x2 = (((state.2 << Lfsr258.q.2) ^ state.2) >> (l - Lfsr258.k.2 - Lfsr258.s.2))
-		state.2 = ((state.2 & (UInt64.max << Lfsr258.k.2)) << Lfsr258.s.2) | x2
-		let x3 = (((state.3 << Lfsr258.q.3) ^ state.3) >> (l - Lfsr258.k.3 - Lfsr258.s.3))
-		state.3 = ((state.3 & (UInt64.max << Lfsr258.k.3)) << Lfsr258.s.3) | x3
-		let x4 = (((state.4 << Lfsr258.q.4) ^ state.4) >> (l - Lfsr258.k.4 - Lfsr258.s.4))
-		state.4 = ((state.4 & (UInt64.max << Lfsr258.k.4)) << Lfsr258.s.4) | x4
-		return (state.0 ^ state.1 ^ state.2 ^ state.3 ^ state.4)
-	}
-}
-
-public struct Lfsr176: RandomWordGenerator {
-	public typealias WordType = UInt64
-	public typealias StateType = (UInt64, UInt64, UInt64)
-
-	static let k: (UInt64, UInt64, UInt64) = (1, 6, 9)
-	static let q: (UInt64, UInt64, UInt64) = (5, 19, 24)
-	static let s: (UInt64, UInt64, UInt64) = (24, 13, 17)
-
-	var state: StateType = (0, 0, 0)
-
-	public init() {
-		var r = DevRandom()
-		repeat {
-			r.randomize(buffer: &state.0, size: MemoryLayout<UInt64>.size)
-		} while state.0 < Lfsr176.k.0
-		repeat {
-			r.randomize(buffer: &state.1, size: MemoryLayout<UInt64>.size)
-		} while state.1 < Lfsr176.k.1
-		repeat {
-			r.randomize(buffer: &state.2, size: MemoryLayout<UInt64>.size)
-		} while state.2 < Lfsr176.k.2
-	}
-	
-	public init(seed: StateType) {
-		self.state = seed
-	}
-	
-	public mutating func random64() -> UInt64 {
-		return randomWord()
-	}
-
-	public mutating func randomWord() -> UInt64 {
-		// Constants from "Tables of Maximally-Equidistributed Combined LFSR Generators" by Pierre L'Ecuyer:
-		// http://www.iro.umontreal.ca/~lecuyer/myftp/papers/tausme2.ps
-		let l: UInt64 = 64
-		let x0 = (((state.0 << Lfsr176.q.0) ^ state.0) >> (l - Lfsr176.k.0 - Lfsr176.s.0))
-		state.0 = ((state.0 & (UInt64.max << Lfsr176.k.0)) << Lfsr176.s.0) | x0
-		let x1 = (((state.1 << Lfsr176.q.1) ^ state.1) >> (l - Lfsr176.k.1 - Lfsr176.s.1))
-		state.1 = ((state.1 & (UInt64.max << Lfsr176.k.1)) << Lfsr176.s.1) | x1
-		let x2 = (((state.2 << Lfsr176.q.2) ^ state.2) >> (l - Lfsr176.k.2 - Lfsr176.s.2))
-		state.2 = ((state.2 & (UInt64.max << Lfsr176.k.2)) << Lfsr176.s.2) | x2
-		return (state.0 ^ state.1 ^ state.2)
-	}
-}
-
 public struct Xoroshiro: RandomWordGenerator {
 	public typealias WordType = UInt64
 	public typealias StateType = (UInt64, UInt64)
@@ -320,26 +220,6 @@ public struct Xoroshiro: RandomWordGenerator {
 		state.0 = ((state.0 << k0) | (state.0 >> (l - k0))) ^ x ^ (x << k1)
 		state.1 = (x << k2) | (x >> (l - k2))
 		return result
-	}
-}
-
-public struct ConstantNonRandom: RandomWordGenerator {
-	public typealias WordType = UInt64
-	var state: UInt64 = DevRandom.random64()
-
-	public init() {
-	}
-
-	public init(seed: UInt64) {
-		self.state = seed
-	}
-
-	public mutating func random64() -> UInt64 {
-		return randomWord()
-	}
-
-	public mutating func randomWord() -> UInt64 {
-		return state
 	}
 }
 
