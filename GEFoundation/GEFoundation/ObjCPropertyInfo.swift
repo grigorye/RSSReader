eaaf31d29be68aeecd2ed74ba6862c33ad408055
Grigory Entin <grigory.entin@gmail.com>
2017-09-13 03:01:16 +0200

    Updated for Xcode 9.

--- GEFoundation/GEFoundation/ObjCPropertyInfo.swift
+++ GEFoundation/GEFoundation/ObjCPropertyInfo.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public func objCDefaultSetterName(forPropertyName propertyName: String) -> String {
-	return "set\(propertyName.uppercased().characters.first!)\(propertyName.substring(from: propertyName.index(after: propertyName.startIndex))):"
+	return "set\(propertyName.uppercased().characters.first!)\(propertyName[propertyName.index(after: propertyName.startIndex)...]):"
 }
 
 struct PropertyInfo {
