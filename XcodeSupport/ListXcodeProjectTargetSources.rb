8fb87155d973f630899033a17a6ad3a27557c0bb
Grigory Entin <grigory.entin@gmail.com>
2017-12-02 04:19:42 +0100

    Added filtering sources in BUILT_PRODUCTS_DIR.

--- XcodeSupport/ListXcodeProjectTargetSources.rb
+++ XcodeSupport/ListXcodeProjectTargetSources.rb
@@ -6,7 +6,9 @@ targetName = ARGV[1]
 project = Xcodeproj::Project.open(project_path)
 
 target = project.targets.find {|t| t.name == targetName}
-files = target.source_build_phase.files.to_a.map do |pbx_build_file|
+files = target.source_build_phase.files.to_a.select do |pbx_build_file|
+  pbx_build_file.file_ref.source_tree != "BUILT_PRODUCTS_DIR"
+end.map do |pbx_build_file|
   pbx_build_file.file_ref.real_path.to_s
 end
 
