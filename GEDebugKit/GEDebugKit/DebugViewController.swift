fbe9781fd5c601fde345300c95aa2c2492687e17
Grigorii Entin <grigory.entin@gmail.com>
2017-12-06 02:31:41 +0100

    Made liveObjectsForSubclasses a function; switched to multiple parent classes for allocation tracking.

--- GEDebugKit/GEDebugKit/DebugViewController.swift
+++ GEDebugKit/GEDebugKit/DebugViewController.swift
@@ -10,47 +10,57 @@ import GEUIKit
 import GETracing
 import FBAllocationTracker
 
+private func liveObjectsForSubclasses(of parentClasses: [AnyClass]) -> Int {
+    
+    guard let allocationTrackerManager = FBAllocationTrackerManager.shared() else {
+        assert(false)
+        return 0
+    }
+    
+    guard let allocationSummaries = allocationTrackerManager.currentAllocationSummary() else {
+        assert(false)
+        return 0
+    }
+    
+    let subclassSummaries = allocationSummaries.filter {
+        
+        guard let cls = NSClassFromString($0.className) else {
+            assert(false)
+            return false
+        }
+        for parentClass in parentClasses {
+            if cls.isSubclass(of: parentClass) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    let clsAndLive = subclassSummaries.map { ($0.className, $0.aliveObjects) }
+    x$(clsAndLive)
+    
+    let liveObjects = subclassSummaries.reduce(0, {
+        let summary = $1
+        let aliveObjects = summary.aliveObjects
+        //assert(0 < aliveObjects)
+        return $0 + aliveObjects
+    })
+    return liveObjects
+}
+
 class DebugViewController : AccessibilityAwareStaticTableViewController {
     
     @IBOutlet var numberOfObjectsLabel: UILabel!
-    
-    func liveObjectsForSubclasses(of parentClass: AnyClass) -> Int {
-        
-        guard let allocationTrackerManager = FBAllocationTrackerManager.shared() else {
-            assert(false)
-            return 0
-        }
-        
-        guard let allocationSummaries = allocationTrackerManager.currentAllocationSummary() else {
-            assert(false)
-            return 0
-        }
-        
-        let subclassSummaries = allocationSummaries.filter {
-            
-            guard let cls = NSClassFromString($0.className) else {
-                assert(false)
-                return false
-            }
-            return cls.isSubclass(of: parentClass)
-        }
-        let clsAndLive = subclassSummaries.map { ($0.className, $0.aliveObjects) }
-        x$(clsAndLive)
-        
-        let liveObjects = subclassSummaries.reduce(0, {
-            let summary = $1
-            let aliveObjects = summary.aliveObjects
-            //assert(0 < aliveObjects)
-            return $0 + aliveObjects
-        })
-        return liveObjects
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
         
-        let liveObjects = liveObjectsForSubclasses(of: UIViewController.self)
+        let parentClassesForAllocationTracking: [AnyClass] = [
+            UIResponder.self
+        ]
+        
+        let liveObjects = liveObjectsForSubclasses(of: parentClassesForAllocationTracking)
         numberOfObjectsLabel?.text = "\(liveObjects)"
     }
 }
