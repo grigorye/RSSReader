a0d6e7b82e43a879d7b4f13994c985044b8476b3
Grigorii Entin <grigory.entin@gmail.com>
2017-12-07 07:18:17 +0100

    Added initialize/configureDebug.

--- GEDebugKit/GEDebugKit/Debugging.swift
+++ GEDebugKit/GEDebugKit/Debugging.swift
@@ -7,6 +7,10 @@
 //
 
 import GEFoundation
+import GETracing
+import FBAllocationTracker
+import FBMemoryProfiler
+import Loggy
 import Foundation
 
 private let debugError = NSError(domain: "com.grigorye.debug", code: 1)
@@ -19,4 +23,54 @@ public func forceDebugCrash() {
 public func triggerDebugError() {
 	
 	trackError(debugError)
+}
+
+private var retainedObjects: [AnyObject] = []
+
+public func initializeAllocationTracking() {
+    var scope = Activity("Initializing Allocation Tracking").enter(); defer { scope.leave() }
+    guard let allocationTrackerManager = x$(FBAllocationTrackerManager.shared()) else {
+        return
+    }
+    allocationTrackerManager.startTrackingAllocations()
+    allocationTrackerManager.enableGenerations()
+}
+
+public func configureDebug() {
+    
+    if _0 {
+        if defaults.memoryProfilerEnabled {
+            let memoryProfiler = FBMemoryProfiler()
+            memoryProfiler.enable()
+            retainedObjects += [memoryProfiler]
+        }
+    }
+    else {
+        var memoryProfiler: FBMemoryProfiler!
+        retainedObjects += [
+            defaults.observe(\.memoryProfilerEnabled, options: .initial) { (_, _) in
+                if defaults.memoryProfilerEnabled {
+                    guard nil == memoryProfiler else {
+                        return
+                    }
+                    memoryProfiler = FBMemoryProfiler()
+                    memoryProfiler.enable()
+                }
+                else {
+                    guard nil != memoryProfiler else {
+                        return
+                    }
+                    memoryProfiler.disable()
+                    memoryProfiler = nil
+                }
+            }
+        ]
+    }
+}
+
+public func initializeDebug() {
+    
+    if defaults.allocationTrackingEnabled {
+        initializeAllocationTracking()
+    }
 }
