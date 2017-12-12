090db388ae4e0f118a42e4d04847ffa64e0a1943
Grigorii Entin <grigory.entin@gmail.com>
2017-12-12 08:57:33 +0100

    Switched to systemFontSize.

--- GEUIKit/GEUIKit/MonospacedLabel.swift
+++ GEUIKit/GEUIKit/MonospacedLabel.swift
@@ -14,7 +14,7 @@ public class MonospacedLabel : UILabel {
         
         super.init(coder: aDecoder)
         
-        let nonscaledFont = UIFont(name: "Menlo-Regular", size: UIFont.labelFontSize)!
+        let nonscaledFont = UIFont(name: "Menlo-Regular", size: UIFont.systemFontSize)!
         let metrics = UIFontMetrics(forTextStyle: UIFontTextStyle.body)
         font = metrics.scaledFont(for: nonscaledFont)
     }
