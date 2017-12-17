635c1973c1d70591f4d394f4695246b593ce7a6f
Grigory Entin <grigory.entin@gmail.com>
2017-12-17 20:52:59 +0100

    Added FPSMonitoring.swift.

---
+++ GEDebugKit/GEDebugKit/FPSMonitoring.swift
@@ -0,0 +1,95 @@
+//
+//  FPSMonitoring.swift
+//  GEDebugKit
+//
+//  Created by Grigorii Entin on 17/12/2017.
+//  Copyright © 2017 Grigory Entin. All rights reserved.
+//
+
+import FPSCounter
+import Foundation
+
+class FPSMonitor: NSObject, FPSCounterDelegate {
+    
+    lazy var fpsCounter = FPSCounter() … {
+        
+        $0.delegate = self
+    }
+    
+    private lazy var label: UILabel = UILabel() … {
+        
+        $0.font = UIFont.boldSystemFont(ofSize: 12.0)
+        $0.textAlignment = .center
+        $0.text = stringToDisplayForFPS(99)
+        $0.sizeToFit()
+    }
+    
+    // MARK: -
+    
+    func show() {
+        
+        do {
+            let application = UIApplication.shared
+            
+            let window = UIWindow(statusBarOverlay: label)
+            let detachHandler = application.makeWindowOverlayStatusBar(window)
+            
+            scheduledForHide.append(detachHandler)
+        }
+
+        do {
+            fpsCounter.startTracking()
+            
+            scheduledForHide.append {
+                self.fpsCounter.stopTracking()
+            }
+        }
+    }
+    
+    var scheduledForHide = ScheduledHandlers()
+    
+    func hide() {
+        
+        scheduledForHide.perform()
+    }
+    
+    // MARK: - FPSCounterDelegate
+    
+    func stringToDisplayForFPS(_ fps: Int) -> String {
+        
+        return String.localizedStringWithFormat(
+            NSLocalizedString("%d", comment: ""),
+            fps
+        )
+    }
+
+    func fpsCounter(_ counter: FPSCounter, didUpdateFramesPerSecond fps: Int) {
+        
+        let colors: (background: UIColor, text: UIColor) = {
+            
+            if fps >= 45 {
+                return (
+                    background: .green,
+                    text: .black
+                )
+            } else if fps >= 30 {
+                return (
+                    background: .orange,
+                    text: .white
+                )
+            } else {
+                return (
+                    background: .red,
+                    text: .white
+                )
+            }
+        }()
+
+        label … {
+            
+            $0.text = stringToDisplayForFPS(fps)
+            $0.textColor = colors.text
+            $0.backgroundColor = colors.background
+        }
+    }
+}
