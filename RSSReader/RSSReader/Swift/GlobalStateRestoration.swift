2b50e11dc27e2279d54b098e0516994641a25ae3
Grigory Entin <grigory.entin@gmail.com>
2018-01-04 13:36:40 +0100

    Added state restoration indicator.

--- RSSReader/RSSReader/Swift/GlobalStateRestoration.swift
+++ RSSReader/RSSReader/Swift/GlobalStateRestoration.swift
@@ -6,17 +6,27 @@
 //  Copyright © 2016 Grigory Entin. All rights reserved.
 //
 
+import JGProgressHUD
 import Loggy
 import UIKit
 
 extension TypedUserDefaults {
 	@NSManaged var stateRestorationEnabled: Bool
+	@NSManaged var stateRestorationIndicatorEnabled: Bool
 }
 
 private var activity = Activity("State Restoration")
 var activityScope: Activity.Scope!
 
 private let currentRestorationFormatVersion: Int32 = 2
+
+private let stateRestorationHud = JGProgressHUD(style: .light) … {
+	
+	$0.textLabel.text = "State Restored"
+	$0.indicatorView = JGProgressHUDSuccessIndicatorView()
+}
+
+private var scheduledForDidDecodeRestorableState = ScheduledHandlers()
 
 extension AppDelegate {
 	private enum Restorable: String {
@@ -27,16 +37,39 @@ extension AppDelegate {
 		coder.encode(currentRestorationFormatVersion, forKey: Restorable.restorationFormatVersion.rawValue)
 		return true
 	}
+	
 	func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+		
 		activityScope = activity.enter()
+		scheduledForDidDecodeRestorableState.append {
+			
+			activityScope.leave()
+		}
+		
 		x$(self)
+		
 		let restorationFormatVersion = coder.decodeInt32(forKey: Restorable.restorationFormatVersion.rawValue)
 		if x$(restorationFormatVersion) < currentRestorationFormatVersion {
 			return false
 		}
-		return x$(defaults.stateRestorationEnabled)
+		
+		let stateRestorationEnabled = x$(defaults.stateRestorationEnabled)
+
+		if stateRestorationEnabled, defaults.stateRestorationIndicatorEnabled {
+			
+			scheduledForDidDecodeRestorableState.append {
+				DispatchQueue.main.async {
+					stateRestorationHud.show(in: UIApplication.shared.keyWindow!)
+					stateRestorationHud.dismiss(afterDelay: 1)
+				}
+			}
+		}
+		
+		return stateRestorationEnabled
 	}
+	
 	func application(_ application: UIApplication, didDecodeRestorableStateWith coder: NSCoder) {
-		activityScope.leave()
+		
+		scheduledForDidDecodeRestorableState.perform()
 	}
 }
