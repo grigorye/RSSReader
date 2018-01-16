600175a2b5ec84259f658c1059e151c2d9983396
Grigory Entin <grigory.entin@gmail.com>
2018-01-16 03:31:36 +0100

    Segregated RSSData/rssData.

--- RSSReaderData/RSSReaderData/CoreDataGlobals.swift
+++ RSSReaderData/RSSReaderData/CoreDataGlobals.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import class GECoreData.PersistentContainerWithCustomDirectory
 import CoreData
 
 extension TypedUserDefaults {
@@ -17,154 +18,209 @@ extension TypedUserDefaults {
 	@NSManaged var savingDisabled: Bool
 	@NSManaged var persistentContainerEnabled: Bool
 	@NSManaged var multipleBackgroundContextsEnabled: Bool
-
 }
 
-let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: NSClassFromString("RSSReaderData.Folder")!)])!
+public class RSSData {
+	
+	static let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: NSClassFromString("RSSReaderData.Folder")!)])!
 
-private func regeneratedPSC() throws -> NSPersistentStoreCoordinator {
-	let psc = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
-	let fileManager = FileManager.default
-	let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
-	let documentsDirectoryURL = urls[0]
-	if !fileManager.fileExists(atPath: documentsDirectoryURL.path) {
-		try fileManager.createDirectory(at: documentsDirectoryURL, withIntermediateDirectories: true, attributes: nil)
-	}
-	let storeURL = documentsDirectoryURL.appendingPathComponent("RSSReaderData.sqlite")
-	x$(fileManager.fileExists(atPath: storeURL.path))
-	if defaults.forceStoreRemoval {
+	private static func regeneratedPSC() throws -> NSPersistentStoreCoordinator {
+		let psc = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
 		let fileManager = FileManager.default
-		do {
-			try fileManager.removeItem(at: storeURL)
+		let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+		let documentsDirectoryURL = urls[0]
+		if !fileManager.fileExists(atPath: documentsDirectoryURL.path) {
+			try fileManager.createDirectory(at: documentsDirectoryURL, withIntermediateDirectories: true, attributes: nil)
 		}
-		catch CocoaError.fileNoSuchFile {
-		}
-	}
-	do {
-		let options = [
-			NSMigratePersistentStoresAutomaticallyOption: true,
-			NSInferMappingModelAutomaticallyOption: true
-		]
-		_ = try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
-	} catch let error as CocoaError {
-		switch error.code.rawValue {
-		case NSMigrationMissingSourceModelError where UserDefaults().bool(forKey: "allowMissingSourceModelError"):
-			fallthrough
-		case NSPersistentStoreIncompatibleVersionHashError, NSMigrationError:
+		let storeURL = documentsDirectoryURL.appendingPathComponent("RSSReaderData.sqlite")
+		x$(fileManager.fileExists(atPath: storeURL.path))
+		if defaults.forceStoreRemoval {
 			let fileManager = FileManager.default
-			try fileManager.removeItem(at: storeURL)
-			try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
-		default:
-			throw error
+			do {
+				try fileManager.removeItem(at: storeURL)
+			}
+			catch CocoaError.fileNoSuchFile {
+			}
+		}
+		do {
+			let options = [
+				NSMigratePersistentStoresAutomaticallyOption: true,
+				NSInferMappingModelAutomaticallyOption: true
+			]
+			_ = try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
+		} catch let error as CocoaError {
+			switch error.code.rawValue {
+			case NSMigrationMissingSourceModelError where UserDefaults().bool(forKey: "allowMissingSourceModelError"):
+				fallthrough
+			case NSPersistentStoreIncompatibleVersionHashError, NSMigrationError:
+				let fileManager = FileManager.default
+				try fileManager.removeItem(at: storeURL)
+				try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+			default:
+				throw error
+			}
+		}
+		return psc
+	}
+
+	private var saveQueueMOCAutosaver: ManagedObjectContextAutosaver?
+	private let (managedObjectContextError, saveQueueManagedObjectContext): (Error?, NSManagedObjectContext?) = {
+		do {
+			let saveQueueManagedObjectContext = try NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
+				$0.name = "save"
+				$0.persistentStoreCoordinator = try regeneratedPSC()
+			}
+			return (nil, saveQueueManagedObjectContext)
+		}
+		catch {
+			return (error, nil)
+		}
+	}()
+
+	lazy private (set) var persistentMainQueueManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)…{
+		$0.name = "main"
+		$0.parent = saveQueueManagedObjectContext
+	}
+
+	func newBoundMainQueueManagedObjectContext() -> NSManagedObjectContext {
+		guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
+			return persistentMainQueueManagedObjectContext
+		}
+		assert(0 < persistentContainer.persistentStoreCoordinator.persistentStores.count)
+		return persistentContainer.viewContext
+	}
+
+	private var mainQueueMOCAutosaver: ManagedObjectContextAutosaver?
+
+	private (set) lazy var mainQueueManagedObjectContext: NSManagedObjectContext = newBoundMainQueueManagedObjectContext() … {
+		
+		if !defaults.savingDisabled {
+			mainQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: $0, queue: nil)
 		}
 	}
-	return psc
-}
 
-private var saveQueueMOCAutosaver: ManagedObjectContextAutosaver?
-private let (managedObjectContextError, saveQueueManagedObjectContext): (Error?, NSManagedObjectContext?) = {
-	do {
-		let saveQueueManagedObjectContext = try NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
-			$0.name = "save"
-			$0.persistentStoreCoordinator = try regeneratedPSC()
+	func newBackgroundQueueManagedObjectContext() -> NSManagedObjectContext {
+
+		guard defaults.backgroundImportEnabled else {
+			return mainQueueManagedObjectContext
 		}
-		return (nil, saveQueueManagedObjectContext)
-	}
-	catch {
-		return (error, nil)
-	}
-}()
-
-let persistentMainQueueManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)…{
-	$0.name = "main"
-	$0.parent = saveQueueManagedObjectContext
-}
-
-func newBoundMainQueueManagedObjectContext() -> NSManagedObjectContext {
-    guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
-        return persistentMainQueueManagedObjectContext
-    }
-    assert(0 < persistentContainer.persistentStoreCoordinator.persistentStores.count)
-    return persistentContainer.viewContext
-}
-
-private var mainQueueMOCAutosaver: ManagedObjectContextAutosaver?
-
-public let mainQueueManagedObjectContext: NSManagedObjectContext = newBoundMainQueueManagedObjectContext() … {
-    
-    if !defaults.savingDisabled {
-        mainQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: $0, queue: nil)
-    }
-}
-
-func newBackgroundQueueManagedObjectContext() -> NSManagedObjectContext {
-
-    guard defaults.backgroundImportEnabled else {
-        return mainQueueManagedObjectContext
-    }
-    guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
-        return NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
-            $0.name = "background"
-            $0.parent = mainQueueManagedObjectContext
-            if defaults.coreDataCachingEnabled {
-                $0.cachingEnabled = true
-            }
-        }
-    }
-    return persistentContainer.newBackgroundContext() … {
-        $0.name = "background"
-    }
-}
-
-let backgroundQueueManagedObjectContext: NSManagedObjectContext = newBackgroundQueueManagedObjectContext() … {
-    
-    if !defaults.savingDisabled {
-        saveQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: $0, queue: nil)
-    }
-}
-
-public func performBackgroundMOCTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
-	guard #available(iOS 10.0, *), defaults.persistentContainerEnabled, defaults.multipleBackgroundContextsEnabled else {
-		return backgroundQueueManagedObjectContext.perform {
-			task(backgroundQueueManagedObjectContext)
+		guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
+			return NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) … {
+				$0.name = "background"
+				$0.parent = mainQueueManagedObjectContext
+				if defaults.coreDataCachingEnabled {
+					$0.cachingEnabled = true
+				}
+			}
+		}
+		return persistentContainer.newBackgroundContext() … {
+			$0.name = "background"
 		}
 	}
-	return persistentContainer.performBackgroundTask { context in
-		context.name = "background"
-		task(context)
+
+	private (set) lazy var backgroundQueueManagedObjectContext: NSManagedObjectContext = newBackgroundQueueManagedObjectContext() … {
+		
+		if !defaults.savingDisabled {
+			saveQueueMOCAutosaver = ManagedObjectContextAutosaver(managedObjectContext: $0, queue: nil)
+		}
+	}
+
+	func performBackgroundMOCTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
+		guard #available(iOS 10.0, *), defaults.persistentContainerEnabled, defaults.multipleBackgroundContextsEnabled else {
+			let backgroundQueueManagedObjectContext = self.backgroundQueueManagedObjectContext
+			return backgroundQueueManagedObjectContext.perform {
+				task(backgroundQueueManagedObjectContext)
+			}
+		}
+		return persistentContainer.performBackgroundTask { context in
+			context.name = "background"
+			task(context)
+		}
+	}
+
+	let persistentContainer = PersistentContainerWithCustomDirectory(name: "RSSReader", managedObjectModel: managedObjectModel)
+
+	func loadPersistentStores(completionHandler: @escaping (Error?) -> ()) {
+		guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
+			completionHandler(managedObjectContextError)
+			return
+		}
+		persistentContainer.loadPersistentStoresAndWait { [weak persistentContainer] error in
+			guard let persistentContainer = persistentContainer else {
+				return
+			}
+			guard nil == error else {
+				completionHandler(error!)
+				return
+			}
+			guard !defaults.forceStoreRemoval else {
+				let context = persistentContainer.viewContext
+				context.perform {
+					try! context.removeAllObjects(for: persistentContainer.managedObjectModel.entities)
+					try! context.save()
+					completionHandler(nil)
+				}
+				return
+			}
+			completionHandler(nil)
+		}
+	}
+
+	func erasePersistentStores() throws {
+		guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
+			assert(false)
+			return
+		}
+		for storeDescription in persistentContainer.persistentStoreDescriptions {
+			let fileManager = FileManager()
+			// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3"
+			// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3-shm"
+			// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3-wal"
+			// "RSSReader.sqlite"
+			// "RSSReader.sqlite-shm"
+			// "RSSReader.sqlite-wal"
+			let storeBasenameURL = storeDescription.url!.deletingPathExtension()
+			for pathExtension in ["sqlite", "sqlite-shm", "sqlite-wal"] {
+				let storeFileURL = storeBasenameURL.appendingPathExtension(pathExtension)
+				try fileManager.removeItem(at: storeFileURL)
+			}
+		}
 	}
 }
 
-private func configureViewContext(_ viewContext: NSManagedObjectContext) {
-	viewContext … {
-		$0.name = "view"
-		$0.automaticallyMergesChangesFromParent = true
-	}
-}
-
-@available (iOS 10.0, *)
-let persistentContainer = NSPersistentContainer(name: "RSSReader", managedObjectModel: managedObjectModel)
-
-@available (iOS 10.0, *)
-struct LoadPersistentStoresError : Error {
-	let errorsAndDescriptions: [(Error, NSPersistentStoreDescription)]
-}
-
-@available (iOS 10.0, *)
 extension NSPersistentContainer {
-
+	
+	struct LoadPersistentStoresError : Error {
+		let errorsAndDescriptions: [(Error, NSPersistentStoreDescription)]
+	}
+	
+	private func configureViewContext(_ viewContext: NSManagedObjectContext) {
+		viewContext … {
+			$0.name = "view"
+			$0.automaticallyMergesChangesFromParent = true
+		}
+	}
+	
+	func completeLoadPersistentStoresAndWait(_ completionHandler: @escaping (Error?) -> Void) {
+		configureViewContext(viewContext)
+		completionHandler(nil)
+	}
+	
 	func loadPersistentStoresAndWait(completionHandler: @escaping (Error?) -> Void) {
 		if defaults.forceStoreRemoval {
-			for storeDescription in persistentContainer.persistentStoreDescriptions {
+			for storeDescription in persistentStoreDescriptions {
 				x$(storeDescription.url)
 			}
 		}
-		var descriptionsToComplete = persistentContainer.persistentStoreDescriptions.count
+		var descriptionsToComplete = persistentStoreDescriptions.count
 		var errorsAndDescriptions = [(Error, NSPersistentStoreDescription)]()
 		let completionQueue = DispatchQueue(label: "loadPersistentStoresCompletion")
 		loadPersistentStores { (description, error) in
 			x$(description)
-			completionQueue.async {
+			completionQueue.async { [weak self] in
+				guard let pself = self else {
+					return
+				}
 				if let error = error {
 					errorsAndDescriptions += [(error, description)]
 				}
@@ -176,16 +232,15 @@ extension NSPersistentContainer {
 					completionHandler(LoadPersistentStoresError(errorsAndDescriptions: errorsAndDescriptions))
 					return
 				}
-				configureViewContext(self.viewContext)
-				completionHandler(nil)
+				pself.completeLoadPersistentStoresAndWait(completionHandler)
 			}
 		}
 	}
-	
 }
 
-extension NSManagedObjectContext {
 
+extension NSManagedObjectContext {
+	
 	func removeAllObjects(for entities: [NSEntityDescription]) throws {
 		for entity in entities {
 			let fetchRequest = NSFetchRequest<NSFetchRequestResult>() … {
@@ -197,48 +252,41 @@ extension NSManagedObjectContext {
 		}
 	}
 	
+	func removeAllObjects() throws {
+		guard let persistentStoreCoordinator = persistentStoreCoordinator else {
+			return
+		}
+		try removeAllObjects(for: persistentStoreCoordinator.managedObjectModel.entities)
+	}
+}
+
+var rssDataImp: RSSData? = nil
+
+var rssData: RSSData {
+	guard let rssDataImp = rssDataImp else {
+		let rssDataImp = RSSData()
+		RSSReaderData.rssDataImp = rssDataImp
+		return rssDataImp
+	}
+	return rssDataImp
+}
+
+public var mainQueueManagedObjectContext: NSManagedObjectContext {
+	
+	return rssData.mainQueueManagedObjectContext
+}
+
+public func performBackgroundMOCTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
+	
+	rssData.performBackgroundMOCTask(task)
 }
 
 public func loadPersistentStores(completionHandler: @escaping (Error?) -> ()) {
-	guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
-		completionHandler(managedObjectContextError)
-		return
-	}
-	persistentContainer.loadPersistentStoresAndWait { error in
-		guard nil == error else {
-			completionHandler(error!)
-			return
-		}
-		guard !defaults.forceStoreRemoval else {
-			let context = persistentContainer.viewContext
-			context.perform {
-				try! context.removeAllObjects(for: persistentContainer.managedObjectModel.entities)
-				try! context.save()
-				completionHandler(nil)
-			}
-			return
-		}
-		completionHandler(nil)
-	}
+	
+	rssData.loadPersistentStores(completionHandler: completionHandler)
 }
 
 public func erasePersistentStores() throws {
-	guard #available(iOS 10.0, *), defaults.persistentContainerEnabled else {
-		assert(false)
-		return
-	}
-	for storeDescription in persistentContainer.persistentStoreDescriptions {
-		let fileManager = FileManager()
-		// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3"
-		// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3-shm"
-		// ".RSSReader.sqlite.migrationdestination_41b5a6b5c6e848c462a8480cd24caef3-wal"
-		// "RSSReader.sqlite"
-		// "RSSReader.sqlite-shm"
-		// "RSSReader.sqlite-wal"
-		let storeBasenameURL = storeDescription.url!.deletingPathExtension()
-		for pathExtension in ["sqlite", "sqlite-shm", "sqlite-wal"] {
-			let storeFileURL = storeBasenameURL.appendingPathExtension(pathExtension)
-			try fileManager.removeItem(at: storeFileURL)
-		}
-	}
+	
+	try rssData.erasePersistentStores()
 }
