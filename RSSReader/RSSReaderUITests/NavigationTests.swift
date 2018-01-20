9c5e34ddf2d68526d1d1fd6c221f1e9cc31033c9
Grigory Entin <grigory.entin@gmail.com>
2018-01-20 16:57:33 +0100

    Renamed RSSReaderUITests into NavigationTests; updated for renamed TestsBase.

--- RSSReader/RSSReaderUITests/NavigationTests.swift
+++ RSSReader/RSSReaderUITests/NavigationTests.swift
@@ -1,5 +1,5 @@
 //
-//  RSSReaderUITests.swift
+//  NavigationTests.swift
 //  RSSReaderUITests
 //
 //  Created by Grigory Entin on 15.06.15.
@@ -9,46 +9,8 @@
 import Foundation
 import XCTest
 
-class RSSReaderUITests : RSSReaderUITestsBase {
+class NavigationTests : TestsBase {
 
-	var app = XCUIApplication()
-	lazy var tablesQuery: XCUIElementQuery = self.app.tables
-	lazy var backButton: XCUIElement = self.app.navigationBars.element(boundBy: 0).buttons.element(boundBy: 0)
-	
-	var isAtHome: Bool {
-		return tablesQuery.staticTexts["Subscriptions-AI"].exists
-	}
-	
-    func pushAndPopHomeItemWithAI(_ itemAI: String) {
-		let homeItem = tablesQuery.staticTexts[itemAI]
-		homeItem.tap()
-		backButton.tap()
-		XCTAssert(isAtHome)
-	}
-	
-	func pushToFirstStreamInSubscriptions() {
-		let subscriptionsItem = tablesQuery.staticTexts["Subscriptions-AI"]
-		subscriptionsItem.tap()
-		while 0 < tablesQuery.count {
-			tablesQuery.cells.element(boundBy: 0).tap()
-		}
-		XCTAssert(!isAtHome)
-	}
-	
-	func popToRoot() {
-		while !isAtHome {
-			backButton.tap()
-		}
-	}
-	
-	// MARK:-
-	
-	func repeatForTesting(_ block: () -> ()) {
-		for _ in 0..<5 {
-			block()
-		}
-	}
-	
 	// MARK:-
 
 	func testOpenHistory() {
