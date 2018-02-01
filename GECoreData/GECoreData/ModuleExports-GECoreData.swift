6038c8a2ae67796f2daf0f38da0039ad30e20506
Grigory Entin <grigory.entin@gmail.com>
2018-02-01 22:55:18 +0100

    Switched to @_exported.

--- GECoreData/GECoreData/ModuleExports-GECoreData.swift
+++ GECoreData/GECoreData/ModuleExports-GECoreData.swift
@@ -6,24 +6,4 @@
 //  Copyright © 2016 Grigory Entin. All rights reserved.
 //
 
-import GECoreData
-import CoreData
-
-typealias DefaultSortable = GECoreData.DefaultSortable
-typealias ManagedIdentifiable = GECoreData.ManagedIdentifiable
-typealias ManagedObjectContextAutosaver = GECoreData.ManagedObjectContextAutosaver
-typealias FetchedObjectBinding = GECoreData.FetchedObjectBinding
-typealias FetchedObjectCountBinding = GECoreData.FetchedObjectCountBinding
-
-func typedObjectID<T: NSManagedObject>(for object: T) -> TypedManagedObjectID<T> {
-	return GECoreData.typedObjectID(for: object)
-}
-
-func typedObjectID<T: NSManagedObject>(for object: T?) -> TypedManagedObjectID<T>? {
-	return GECoreData.typedObjectID(for: object)
-}
-
-func object<ResultType>(in controller: NSFetchedResultsController<ResultType>, indexedBy delta: Int, from anotherObject: ResultType) -> ResultType? where ResultType : NSManagedObject {
-
-	return GECoreData.object(in: controller, indexedBy: delta, from: anotherObject)
-}
+@_exported import GECoreData
