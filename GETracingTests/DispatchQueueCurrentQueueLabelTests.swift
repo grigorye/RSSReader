ea9bef6b2875b469de05b753d9377b1bc67f3729
Grigory Entin <grigory.entin@gmail.com>
2018-04-08 03:49:47 +0200

    Added DispatchQueueCurrentQueueLabelTests.

---
+++ GETracingTests/DispatchQueueCurrentQueueLabelTests.swift
@@ -0,0 +1,27 @@
+//
+//  DispatchQueueCurrentQueueLabelTests.swift
+//  GETracingTests
+//
+//  Created by Grigory Entin on 08/04/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import GETracing
+import XCTest
+
+class DispatchQueueCurrentQueueLabelTests: XCTestCase {
+	
+	func testExample() {
+		let mainQueueLabel = x$(DispatchQueue.currentQueueLabel)
+		XCTAssertNotNil(mainQueueLabel)
+		var globalQueueLabel: String?
+		let asyncCompleted = expectation(description: "Async completed")
+		DispatchQueue.global().async {
+			globalQueueLabel = x$(DispatchQueue.currentQueueLabel)
+			XCTAssertNotNil(globalQueueLabel)
+			asyncCompleted.fulfill()
+		}
+		waitForExpectations(timeout: 0.1)
+		XCTAssertNotEqual(mainQueueLabel, globalQueueLabel)
+	}
+}
