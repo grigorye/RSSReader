0f3dc73912806967a17b963db3eb2869dbde0ad0
Grigory Entin <grigory.entin@gmail.com>
2018-10-17 00:09:35 +0200

    Switched from ModuleExports-GETracing/GEFoundation to SharedImports.

---
+++ RSSReaderAppConfig/RSSReaderAppConfig/SharedImports.swift
@@ -0,0 +1,10 @@
+//
+//  SharedImports.swift
+//  RSSReaderAppConfig
+//
+//  Created by Grigory Entin on 17/10/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+@_exported import GEFoundation
+@_exported import GETracing
