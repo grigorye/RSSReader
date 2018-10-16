1c86b67178d6999d4977cdd503de77f29499d946
Grigory Entin <grigory.entin@gmail.com>
2018-10-17 00:08:15 +0200

    Switched from ModuleExports-GETracing/GEFoundation to SharedImports.

---
+++ GEDebugKit/GEDebugKit/SharedImports.swift
@@ -0,0 +1,10 @@
+//
+//  SharedImports.swift
+//  GEDebugKit
+//
+//  Created by Grigory Entin on 14/10/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+@_exported import GEFoundation
+@_exported import GETracing
