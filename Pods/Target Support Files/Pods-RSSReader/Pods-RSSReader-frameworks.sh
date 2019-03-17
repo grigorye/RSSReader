04c2833784e9dfb9565accba532fdd0f40ad53b1
Grigory Entin <grigory.entin@gmail.com>
2019-03-17 20:33:31 +0100

    Regenerated pods.

--- Pods/Target Support Files/Pods-RSSReader/Pods-RSSReader-frameworks.sh
+++ Pods/Target Support Files/Pods-RSSReader/Pods-RSSReader-frameworks.sh
@@ -3,8 +3,9 @@
 # ---- this is added by cocoapods-binary ---
 # Readlink cannot handle relative symlink well, so we override it to a new one
 # If the path isn't an absolute path, we add a realtive prefix.
+old_read_link=`which readlink`
 readlink () {
-    path=`/usr/bin/readlink $1`;
+    path=`$old_read_link $1`;
     if [ $(echo "$path" | cut -c 1-1) = '/' ]; then
         echo $path;
     else
@@ -60,8 +61,8 @@ install_framework()
   fi
 
   # Use filter instead of exclude so missing patterns don't throw errors.
-  echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${destination}\""
-  rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${destination}"
+  echo "rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${destination}\""
+  rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${destination}"
 
   local basename
   basename="$(basename -s .framework "$1")"
@@ -100,8 +101,8 @@ install_dsym() {
   local source="$1"
   if [ -r "$source" ]; then
     # Copy the dSYM into a the targets temp dir.
-    echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${DERIVED_FILES_DIR}\""
-    rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${DERIVED_FILES_DIR}"
+    echo "rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${DERIVED_FILES_DIR}\""
+    rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${DERIVED_FILES_DIR}"
 
     local basename
     basename="$(basename -s .framework.dSYM "$source")"
@@ -114,8 +115,8 @@ install_dsym() {
 
     if [[ $STRIP_BINARY_RETVAL == 1 ]]; then
       # Move the stripped file into its final destination.
-      echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${DERIVED_FILES_DIR}/${basename}.framework.dSYM\" \"${DWARF_DSYM_FOLDER_PATH}\""
-      rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${DERIVED_FILES_DIR}/${basename}.framework.dSYM" "${DWARF_DSYM_FOLDER_PATH}"
+      echo "rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${DERIVED_FILES_DIR}/${basename}.framework.dSYM\" \"${DWARF_DSYM_FOLDER_PATH}\""
+      rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${DERIVED_FILES_DIR}/${basename}.framework.dSYM" "${DWARF_DSYM_FOLDER_PATH}"
     else
       # The dSYM was not stripped at all, in this case touch a fake folder so the input/output paths from Xcode do not reexecute this script because the file is missing.
       touch "${DWARF_DSYM_FOLDER_PATH}/${basename}.framework.dSYM"
@@ -127,8 +128,8 @@ install_dsym() {
 install_bcsymbolmap() {
     local bcsymbolmap_path="$1"
     local destination="${BUILT_PRODUCTS_DIR}"
-    echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}""
-    rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}"
+    echo "rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}""
+    rsync --copy-links --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}"
 }
 
 # Signs a framework with the provided identity
@@ -176,45 +177,79 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/FBAllocationTracker/FBAllocationTracker.framework"
+  install_dsym "${PODS_ROOT}/FBAllocationTracker/FBAllocationTracker.framework.dSYM"
   install_framework "${PODS_ROOT}/FBMemoryProfiler/FBMemoryProfiler.framework"
+  install_dsym "${PODS_ROOT}/FBMemoryProfiler/FBMemoryProfiler.framework.dSYM"
   install_framework "${PODS_ROOT}/FBRetainCycleDetector/FBRetainCycleDetector.framework"
+  install_dsym "${PODS_ROOT}/FBRetainCycleDetector/FBRetainCycleDetector.framework.dSYM"
   install_framework "${PODS_ROOT}/FPSCounter/FPSCounter.framework"
+  install_dsym "${PODS_ROOT}/FPSCounter/FPSCounter.framework.dSYM"
   install_framework "${PODS_ROOT}/FTLinearActivityIndicator/FTLinearActivityIndicator.framework"
+  install_dsym "${PODS_ROOT}/FTLinearActivityIndicator/FTLinearActivityIndicator.framework.dSYM"
   install_framework "${PODS_ROOT}/GECoreData/GECoreData.framework"
+  install_dsym "${PODS_ROOT}/GECoreData/GECoreData.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/GEDebugKit/GEDebugKit.framework"
   install_framework "${PODS_ROOT}/GEFoundation/GEFoundation.framework"
+  install_dsym "${PODS_ROOT}/GEFoundation/GEFoundation.framework.dSYM"
   install_framework "${PODS_ROOT}/GETracing/GETracing.framework"
+  install_dsym "${PODS_ROOT}/GETracing/GETracing.framework.dSYM"
   install_framework "${PODS_ROOT}/GEUIKit/GEUIKit.framework"
+  install_dsym "${PODS_ROOT}/GEUIKit/GEUIKit.framework.dSYM"
   install_framework "${PODS_ROOT}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_dsym "${PODS_ROOT}/GoogleToolboxForMac/GoogleToolboxForMac.framework.dSYM"
   install_framework "${PODS_ROOT}/JGProgressHUD/JGProgressHUD.framework"
+  install_dsym "${PODS_ROOT}/JGProgressHUD/JGProgressHUD.framework.dSYM"
   install_framework "${PODS_ROOT}/Loggy/Loggy.framework"
+  install_dsym "${PODS_ROOT}/Loggy/Loggy.framework.dSYM"
   install_framework "${PODS_ROOT}/PromisesObjC/FBLPromises.framework"
+  install_dsym "${PODS_ROOT}/PromisesObjC/FBLPromises.framework.dSYM"
   install_framework "${PODS_ROOT}/PromisesSwift/Promises.framework"
+  install_dsym "${PODS_ROOT}/PromisesSwift/Promises.framework.dSYM"
   install_framework "${PODS_ROOT}/R.swift.Library/Rswift.framework"
+  install_dsym "${PODS_ROOT}/R.swift.Library/Rswift.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/RSSReaderData/RSSReaderData.framework"
   install_framework "${PODS_ROOT}/Result/Result.framework"
+  install_dsym "${PODS_ROOT}/Result/Result.framework.dSYM"
   install_framework "${PODS_ROOT}/Watchdog/Watchdog.framework"
+  install_dsym "${PODS_ROOT}/Watchdog/Watchdog.framework.dSYM"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/FBAllocationTracker/FBAllocationTracker.framework"
+  install_dsym "${PODS_ROOT}/FBAllocationTracker/FBAllocationTracker.framework.dSYM"
   install_framework "${PODS_ROOT}/FBMemoryProfiler/FBMemoryProfiler.framework"
+  install_dsym "${PODS_ROOT}/FBMemoryProfiler/FBMemoryProfiler.framework.dSYM"
   install_framework "${PODS_ROOT}/FBRetainCycleDetector/FBRetainCycleDetector.framework"
+  install_dsym "${PODS_ROOT}/FBRetainCycleDetector/FBRetainCycleDetector.framework.dSYM"
   install_framework "${PODS_ROOT}/FPSCounter/FPSCounter.framework"
+  install_dsym "${PODS_ROOT}/FPSCounter/FPSCounter.framework.dSYM"
   install_framework "${PODS_ROOT}/FTLinearActivityIndicator/FTLinearActivityIndicator.framework"
+  install_dsym "${PODS_ROOT}/FTLinearActivityIndicator/FTLinearActivityIndicator.framework.dSYM"
   install_framework "${PODS_ROOT}/GECoreData/GECoreData.framework"
+  install_dsym "${PODS_ROOT}/GECoreData/GECoreData.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/GEDebugKit/GEDebugKit.framework"
   install_framework "${PODS_ROOT}/GEFoundation/GEFoundation.framework"
+  install_dsym "${PODS_ROOT}/GEFoundation/GEFoundation.framework.dSYM"
   install_framework "${PODS_ROOT}/GETracing/GETracing.framework"
+  install_dsym "${PODS_ROOT}/GETracing/GETracing.framework.dSYM"
   install_framework "${PODS_ROOT}/GEUIKit/GEUIKit.framework"
+  install_dsym "${PODS_ROOT}/GEUIKit/GEUIKit.framework.dSYM"
   install_framework "${PODS_ROOT}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_dsym "${PODS_ROOT}/GoogleToolboxForMac/GoogleToolboxForMac.framework.dSYM"
   install_framework "${PODS_ROOT}/JGProgressHUD/JGProgressHUD.framework"
+  install_dsym "${PODS_ROOT}/JGProgressHUD/JGProgressHUD.framework.dSYM"
   install_framework "${PODS_ROOT}/Loggy/Loggy.framework"
+  install_dsym "${PODS_ROOT}/Loggy/Loggy.framework.dSYM"
   install_framework "${PODS_ROOT}/PromisesObjC/FBLPromises.framework"
+  install_dsym "${PODS_ROOT}/PromisesObjC/FBLPromises.framework.dSYM"
   install_framework "${PODS_ROOT}/PromisesSwift/Promises.framework"
+  install_dsym "${PODS_ROOT}/PromisesSwift/Promises.framework.dSYM"
   install_framework "${PODS_ROOT}/R.swift.Library/Rswift.framework"
+  install_dsym "${PODS_ROOT}/R.swift.Library/Rswift.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/RSSReaderData/RSSReaderData.framework"
   install_framework "${PODS_ROOT}/Result/Result.framework"
+  install_dsym "${PODS_ROOT}/Result/Result.framework.dSYM"
   install_framework "${PODS_ROOT}/Watchdog/Watchdog.framework"
+  install_dsym "${PODS_ROOT}/Watchdog/Watchdog.framework.dSYM"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
